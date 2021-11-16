3916aea7645cfd30a869c311543d07a5b2754a3c
Casey Baggz <caseybaggz@gmail.com>
2021-11-16 14:21:09 -0600

    feat: add eslint & stylint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
+// const OFF = 0
+// const WARN = 1
+// const ERROR = 2
+
 module.exports = {
-  "extends": [
+  root: true,
+  parser: require.resolve('@typescript-eslint/parser'),
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended',
+    // "plugin:jest/recommended",
     // Prettier must be last
-    "prettier"
-  ]
+    'prettier',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'sonarjs',
+    // "jest"
+  ],
 }
