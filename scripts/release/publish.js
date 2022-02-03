15dbd516602892f37c3f2a3dfbda533227594647
Casey Baggz <caseybaggz@gmail.com>
2022-02-03 14:04:20 -0600

    ci(scripts): create publish.js

--- scripts/release/publish.js
+++ scripts/release/publish.js
@@ -2,34 +2,11 @@
 
 'use strict'
 
-const parseParams = require('../publish-commands/parse-params')
-const prepareRelease = require('./prepare-release')
+const parseParams = require('./publish-commands/parse-publish-params')
 
 async function run() {
-  try {
-    const params = await parseParams()
-    const { release } = params
-
-    switch (release) {
-      case 'experimental':
-      case 'alpha':
-      case 'beta':
-      case 'next':
-        prepareRelease(params)
-        break
-
-      case 'stable':
-        prepareRelease()
-        break
-
-      default:
-        process.exit(1)
-        break
-    }
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
+  const params = await parseParams()
+  console.log('publish params', params)
 }
 
 run()
