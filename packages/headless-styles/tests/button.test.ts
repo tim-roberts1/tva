594a110cf7ed4b1e01f7a4b75d296bb0e017c20f
Casey Baggz <caseybaggz@gmail.com>
2022-03-04 09:55:18 -0600

    feat(hs): add test setup

---
+++ packages/headless-styles/tests/button.test.ts
@@ -0,0 +1,9 @@
+import { getButtonProps } from '../src'
+
+describe('button', () => {
+  describe('getButtonProps', () => {
+    test('should exist', () => {
+      expect(getButtonProps).toBeDefined()
+    })
+  })
+})
