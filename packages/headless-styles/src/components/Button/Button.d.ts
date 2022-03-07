071912c8db55e8b0ae15c673283e836fded38e81
Casey Baggz <caseybaggz@gmail.com>
2022-03-07 16:08:47 -0600

    chore(hs): remove unnecessary type file in Button

--- packages/headless-styles/src/components/Button/Button.d.ts
+++
@@ -1,9 +0,0 @@
-interface ButtonOptions {
-  kind: 'default' | 'contained' | 'outlined'
-  size: 'xs' | 's' | 'm' | 'l'
-}
-export declare function getButtonProps(props: ButtonOptions): {
-  className: string
-  type: string
-}
-export {}
