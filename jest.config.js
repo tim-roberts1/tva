eb549e8bf344c4245674f1578e7e75e4d41f2550
Casey Baggz <caseybaggz@gmail.com>
2022-03-21 16:11:45 -0500

    test: add css files support

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
+const cssRegex = '^.+\\.css$'
 const cssModuleRegex = '^.+\\.module\\.css$'
 
 module.exports = {
   moduleNameMapper: {
+    [cssRegex]: 'identity-obj-proxy',
     [cssModuleRegex]: 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
   },
-  transformIgnorePatterns: [`/.pnp.cjs$`, cssModuleRegex],
+  transformIgnorePatterns: [`/.pnp.cjs$`, cssRegex, cssModuleRegex],
   testMatch: [
     `<rootDir>/packages/**/*/?(*.)+(test).(js|ts|tsx)`,
     '!<rootDir>/packages/design-tokens/**/*',
