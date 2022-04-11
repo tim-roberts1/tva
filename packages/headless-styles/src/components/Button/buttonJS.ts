9b24d595dae675fd4963dfba749b1e71ae1de23b
Casey Baggz <caseybaggz@gmail.com>
2022-04-11 15:39:03 -0500

    refactor(hs): cleanup buttonJS.ts & up design-tokens

--- packages/headless-styles/src/components/Button/buttonJS.ts
+++ packages/headless-styles/src/components/Button/buttonJS.ts
@@ -1,109 +1,34 @@
-import {
-  psBackground,
-  psBackgroundActive,
-  psBackgroundHover,
-  psBackgroundWeak,
-  psNeutralBackground,
-  psNeutralBackgroundActive,
-  psNeutralBackgroundHover,
-  psNeutralText,
-  psNeutralTextWeak,
-  psText,
-  psTextMedium,
-} from '@pluralsight/design-tokens'
 import { getDefaultOptions } from './shared'
 import { transformStyles } from '../../utils/helpers'
 import styles from './generated/buttonCSS.module'
-import type { ButtonOptions, ButtonType, Kind } from './types'
+import type { ButtonOptions, ButtonType } from './types'
+
+type ButtonReturn = Record<string, unknown>
 
 const baseStyles = {
   ...styles.base,
-  '&:focus': {
-    outline: `3px solid ${psBackgroundActive}`,
-  },
 }
 
-function getKindStyles(kind: Kind) {
-  switch (kind) {
-    case 'text-weak':
-      return {
-        ...styles.text_weak,
-        color: psTextMedium,
-        '&:active': {
-          backgroundColor: psBackgroundActive,
-        },
-        '&:hover': {
-          backgroundColor: psBackgroundHover,
-          color: '#fff',
-        },
-      }
-
-    case 'weak':
-      return {
-        ...styles.weak,
-        backgroundColor: psNeutralBackground,
-        color: psNeutralText,
-        '&:active': {
-          backgroundColor: psNeutralBackgroundActive,
-        },
-        '&:hover': {
-          backgroundColor: psNeutralBackgroundHover,
-        },
-      }
-
-    case 'medium':
-      return {
-        ...styles.medium,
-        backgroundColor: psBackground,
-        '&:active': {
-          backgroundColor: psBackgroundActive,
-        },
-        '&:hover': {
-          backgroundColor: psBackgroundHover,
-          color: '#fff',
-        },
-      }
-
-    case 'strong':
-      return {
-        ...styles.strong,
-        backgroundColor: psBackgroundWeak,
-        color: psText,
-        '&:active': {
-          backgroundColor: psBackgroundActive,
-        },
-        '&:hover': {
-          backgroundColor: psBackgroundHover,
-          color: '#fff',
-        },
-      }
-
-    default:
-      return {
-        ...styles.text,
-        color: psNeutralTextWeak,
-        '&:active': {
-          backgroundColor: psBackgroundActive,
-        },
-        '&:hover': {
-          backgroundColor: psNeutralBackgroundHover,
-          color: '#fff',
-        },
-      }
-  }
-}
-
-export function getJSButtonProps(options?: ButtonOptions) {
+export function getJSButtonProps(options?: ButtonOptions): ButtonReturn {
   const { kind, size } = getDefaultOptions(options)
-  const kindStyles = getKindStyles(kind)
+  const kindClass = kind.replace('-', '_') as keyof typeof styles
+  const kindKey = styles[kindClass]
   const sizeKey = `size_${size}` as keyof typeof styles
   const JsStyles = {
     ...baseStyles,
-    ...kindStyles,
+    ...kindKey,
     ...styles[sizeKey],
+    '&:hover': {
+      ...(kindKey[
+        '&:hover' as unknown as keyof typeof kindKey
+      ] as typeof styles),
+      color: '#fff',
+    },
     '&:active': {
       ...baseStyles['&:active'],
-      ...kindStyles['&:active'],
+      ...(kindKey[
+        '&:active' as unknown as keyof typeof kindKey
+      ] as typeof styles),
     },
   }
 
