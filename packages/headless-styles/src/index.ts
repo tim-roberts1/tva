8ea32fa58132c15d0ab3434fd1ed92a80deeaf76
Casey Baggz <caseybaggz@gmail.com>
2022-04-20 15:33:32 -0500

    feat(hs): add muiLabelOverride to main exports

--- packages/headless-styles/src/index.ts
+++ packages/headless-styles/src/index.ts
@@ -1,5 +1,5 @@
 export { getBadgeProps } from './components/Badge/badgeCSS'
-export { getJSBadgeProps } from './components/Badge/badgeJS'
+export { getJSBadgeProps, muiLabelOverride } from './components/Badge/badgeJS'
 
 export { getButtonProps } from './components/Button/buttonCSS'
 export { getJSButtonProps } from './components/Button/buttonJS'
