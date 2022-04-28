c7e7a1d4771062fe54f43351049f1f2c8c79037f
Casey Baggz <caseybaggz@gmail.com>
2022-04-28 10:08:35 -0500

    feat(dev): add ChakraButton config

--- packages/headless-styles/src/chakra.ts
+++ packages/headless-styles/src/chakra.ts
@@ -1,9 +1,11 @@
 import { ChakraBadge } from './components/Badge/badgeJS'
+import { ChakraButton } from './components/Button/buttonJS'
 import { ChakraSkeleton } from './components/Skeleton/skeletonJS'
 
 export const chakraPSTheme = {
   components: {
     Badge: ChakraBadge,
+    Button: ChakraButton,
     Skeleton: ChakraSkeleton,
   },
 }
