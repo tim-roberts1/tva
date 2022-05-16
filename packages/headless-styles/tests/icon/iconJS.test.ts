ca579c04f825d94bcd822535c5c86cdc33e9d26d
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-05-16 16:12:34 -0400

    feat(headless-styles): smaller, more definitive test cases

--- packages/headless-styles/tests/icon/iconJS.test.ts
+++ packages/headless-styles/tests/icon/iconJS.test.ts
@@ -6,26 +6,56 @@ describe('icon JS', () => {
     role: 'img',
   }
 
-  test('should return a distinct height and width difference based on the size', () => {
+  test('should accept an "xs" size', () => {
     const xsIconProps = getJSIconProps({ size: 'xs' })
-    const mIconProps = getJSIconProps({ size: 'm' })
+
+    expect(xsIconProps.cssProps).not.toHaveLength(0)
+    expect(xsIconProps.styles).toBeDefined()
+  })
+
+  test('should accept an "s" size', () => {
     const sIconProps = getJSIconProps({ size: 's' })
+
+    expect(sIconProps.cssProps).not.toHaveLength(0)
+    expect(sIconProps.styles).toBeDefined()
+  })
+
+  test('should accept an "m" size', () => {
+    const mIconProps = getJSIconProps({ size: 'm' })
+
+    expect(mIconProps.cssProps).not.toHaveLength(0)
+    expect(mIconProps.styles).toBeDefined()
+  })
+
+  test('should accept an "l" size', () => {
     const lIconProps = getJSIconProps({ size: 'l' })
 
-    expect(xsIconProps.styles.height).not.toEqual(mIconProps.styles.height)
-    expect(xsIconProps.styles.width).not.toEqual(mIconProps.styles.width)
-    expect(xsIconProps.styles.height).not.toEqual(sIconProps.styles.height)
-    expect(xsIconProps.styles.width).not.toEqual(sIconProps.styles.width)
-    expect(xsIconProps.styles.height).not.toEqual(lIconProps.styles.height)
-    expect(xsIconProps.styles.width).not.toEqual(lIconProps.styles.width)
+    expect(lIconProps.cssProps).not.toHaveLength(0)
+    expect(lIconProps.styles).toBeDefined()
+  })
 
-    expect(sIconProps.styles.height).not.toEqual(mIconProps.styles.height)
-    expect(sIconProps.styles.width).not.toEqual(mIconProps.styles.width)
-    expect(sIconProps.styles.height).not.toEqual(lIconProps.styles.height)
-    expect(sIconProps.styles.width).not.toEqual(lIconProps.styles.width)
+  test('should return the appropriate dimensions for "xs" size', () => {
+    const xsIconProps = getJSIconProps({ size: 'xs' })
+    expect(xsIconProps.styles.height).toBe('1rem')
+    expect(xsIconProps.styles.width).toBe('1rem')
+  })
 
-    expect(mIconProps.styles.height).not.toEqual(lIconProps.styles.height)
-    expect(mIconProps.styles.width).not.toEqual(lIconProps.styles.width)
+  test('should return the appropriate dimensions for "s" size', () => {
+    const sIconProps = getJSIconProps({ size: 's' })
+    expect(sIconProps.styles.height).toBe('1.25rem')
+    expect(sIconProps.styles.width).toBe('1.25rem')
+  })
+
+  test('should return the appropriate dimensions for "m" size', () => {
+    const mIconProps = getJSIconProps({ size: 'm' })
+    expect(mIconProps.styles.height).toBe('1.5rem')
+    expect(mIconProps.styles.width).toBe('1.5rem')
+  })
+
+  test('should return the appropriate dimensions for "l" size', () => {
+    const lIconProps = getJSIconProps({ size: 'l' })
+    expect(lIconProps.styles.height).toBe('3rem')
+    expect(lIconProps.styles.width).toBe('3rem')
   })
 
   test('should accept a label', () => {
