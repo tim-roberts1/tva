babbf8eda7908789aec66f71d7fcd74094e6b4d4
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-05-16 16:52:52 -0400

    refactor(headless-styles): make getJSIconProps tests simpler and more definitive

--- packages/headless-styles/tests/icon/iconJS.test.ts
+++ packages/headless-styles/tests/icon/iconJS.test.ts
@@ -1,61 +1,40 @@
 import { getJSIconProps } from '../../src'
+import type { Size } from '../../src/components/Icon/types'
 
 describe('icon JS', () => {
   const baseA11yProps = {
     'aria-hidden': 'false',
     role: 'img',
   }
+  const sizes: Record<Size, string> = {
+    xs: '1rem',
+    s: '1.25rem',
+    m: '1.5rem',
+    l: '3rem',
+  }
 
-  test('should accept an "xs" size', () => {
-    const xsIconProps = getJSIconProps({ size: 'xs' })
-
-    expect(xsIconProps.cssProps).not.toHaveLength(0)
-    expect(xsIconProps.styles).toBeDefined()
-  })
-
-  test('should accept an "s" size', () => {
-    const sIconProps = getJSIconProps({ size: 's' })
-
-    expect(sIconProps.cssProps).not.toHaveLength(0)
-    expect(sIconProps.styles).toBeDefined()
-  })
-
-  test('should accept an "m" size', () => {
-    const mIconProps = getJSIconProps({ size: 'm' })
-
-    expect(mIconProps.cssProps).not.toHaveLength(0)
-    expect(mIconProps.styles).toBeDefined()
-  })
-
-  test('should accept an "l" size', () => {
-    const lIconProps = getJSIconProps({ size: 'l' })
-
-    expect(lIconProps.cssProps).not.toHaveLength(0)
-    expect(lIconProps.styles).toBeDefined()
-  })
+  function testDimensions(size: Size) {
+    const props = getJSIconProps({ size: size })
+    expect(props.styles.height).toEqual(sizes[size])
+    expect(props.styles.width).toEqual(sizes[size])
+    expect(props.cssProps).toContain(`height: ${sizes[size]}`)
+    expect(props.cssProps).toContain(`width: ${sizes[size]}`)
+  }
 
   test('should return the appropriate dimensions for "xs" size', () => {
-    const xsIconProps = getJSIconProps({ size: 'xs' })
-    expect(xsIconProps.styles.height).toBe('1rem')
-    expect(xsIconProps.styles.width).toBe('1rem')
+    testDimensions('xs')
   })
 
   test('should return the appropriate dimensions for "s" size', () => {
-    const sIconProps = getJSIconProps({ size: 's' })
-    expect(sIconProps.styles.height).toBe('1.25rem')
-    expect(sIconProps.styles.width).toBe('1.25rem')
+    testDimensions('s')
   })
 
   test('should return the appropriate dimensions for "m" size', () => {
-    const mIconProps = getJSIconProps({ size: 'm' })
-    expect(mIconProps.styles.height).toBe('1.5rem')
-    expect(mIconProps.styles.width).toBe('1.5rem')
+    testDimensions('m')
   })
 
   test('should return the appropriate dimensions for "l" size', () => {
-    const lIconProps = getJSIconProps({ size: 'l' })
-    expect(lIconProps.styles.height).toBe('3rem')
-    expect(lIconProps.styles.width).toBe('3rem')
+    testDimensions('l')
   })
 
   test('should accept a label', () => {
