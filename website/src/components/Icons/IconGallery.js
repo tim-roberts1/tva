54f6b854d3bf7a37d993061332dd4c17ee46cbb5
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-05-20 13:33:29 -0400

    docs(icons): add categorized gallery

--- website/src/components/Icons/IconGallery.js
+++ website/src/components/Icons/IconGallery.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import { getIconProps } from '@pluralsight/headless-styles'
 import * as psIcons from '@pluralsight/icons'
-import Container from '../Container/Container'
 import styles from './IconGallery.module.css'
 
-const psIconProps = getIconProps()
-const psXSmallIconProps = getIconProps({ size: 'xs' })
-const psSmallIconProps = getIconProps({ size: 's' })
-const psLargeIconProps = getIconProps({ size: 'l' })
-
-const iconNames = Object.keys(psIcons).sort()
-
-const gallery = iconNames.map((key, i) => {
-  const Icon = psIcons[key]
-  const label = key.replace(/[^a-zA-Z0-9]/, '')
+function IconGallery(props) {
+  const { icons, size = 'm' } = props
+  const psIconProps = getIconProps({ size })
 
   return (
-    <span className={styles.example}>
-      <Icon key={`icon_${i}`} {...psIconProps} />
-      <span className={styles.label}>{label}</span>
-    </span>
-  )
-})
+    <div className={styles.gallery}>
+      {Object.keys(icons)
+        .sort()
+        .map((key, i) => {
+          const Icon = psIcons[key]
+          const label = key.replace(/[^a-zA-Z0-9]/, '')
 
-function IconGallery() {
-  return <div className={styles.gallery}>{gallery}</div>
+          return (
+            <span className={styles.example}>
+              <Icon key={`icon_${i}`} {...psIconProps} />
+              <span className={styles.label}>{label}</span>
+            </span>
+          )
+        })}
+    </div>
+  )
 }
 
 export default IconGallery
