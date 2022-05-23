fccd19206601f3470723f04fcf08099a9e9923a0
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-05-23 08:55:37 -0400

    docs(icons): consolidate some icon gallery code and change default size to 'l'

--- website/src/components/Icons/IconGallery.js
+++ website/src/components/Icons/IconGallery.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import { getIconProps } from '@pluralsight/headless-styles'
-import * as psIcons from '@pluralsight/icons'
 import styles from './IconGallery.module.css'
 
 function IconGallery(props) {
-  const { icons, size = 'm' } = props
-  const psIconProps = getIconProps({ size })
+  const { icons, size = 'l' } = props
+  const psIconProps = getIconProps({ size: size })
 
   return (
     <div className={styles.gallery}>
       {Object.keys(icons)
         .sort()
-        .map((key, i) => {
-          const Icon = psIcons[key]
-          const label = key.replace(/[^a-zA-Z0-9]/, '')
+        .map((key) => {
+          const Icon = icons[key]
 
           return (
-            <span className={styles.example}>
-              <Icon key={`icon_${i}`} {...psIconProps} />
-              <span className={styles.label}>{label}</span>
+            <span className={styles.item}>
+              <Icon key={key} {...psIconProps} />
+              <span className={styles.label}>{key}</span>
             </span>
           )
         })}
