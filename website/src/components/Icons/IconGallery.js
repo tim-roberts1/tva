4700d1951d72e2c1213525728f87a2a1ba1171e9
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-05-23 14:42:53 -0400

    refactor(website): refactor icon gallery to use markdown headings

--- website/src/components/Icons/IconGallery.js
+++ website/src/components/Icons/IconGallery.js
@@ -1,27 +1,6 @@
 import React, { memo } from 'react'
 import { getIconProps } from '@pluralsight/headless-styles'
 import styles from './IconGallery.module.css'
-import * as arrowIcons from '@pluralsight/icons/react/arrows'
-import * as avIcons from '@pluralsight/icons/react/audio-video'
-import * as brandIcons from '@pluralsight/icons/react/brands'
-import * as commonIcons from '@pluralsight/icons/react/common-actions'
-import * as communicationIcons from '@pluralsight/icons/react/communication'
-import * as editorIcons from '@pluralsight/icons/react/editor'
-import * as navigationIcons from '@pluralsight/icons/react/navigation'
-import * as socialIcons from '@pluralsight/icons/react/social'
-import * as uiIcons from '@pluralsight/icons/react/ui-actions'
-
-const iconData = {
-  Arrows: arrowIcons,
-  'Audio/Video': avIcons,
-  Brands: brandIcons,
-  'Common Actions': commonIcons,
-  Communication: communicationIcons,
-  Editor: editorIcons,
-  Navigation: navigationIcons,
-  Social: socialIcons,
-  'UI Actions': uiIcons,
-}
 
 function IconListItem(props) {
   const { label, iconComponent } = props
@@ -36,7 +15,7 @@ function IconListItem(props) {
   )
 }
 
-function IconList(props) {
+const IconList = memo(function IconList(props) {
   const { icons } = props
 
   return (
@@ -48,29 +27,6 @@ function IconList(props) {
         ))}
     </ul>
   )
-}
-
-function IconSection(props) {
-  const { category } = props
-
-  return (
-    <section className={styles.section}>
-      <h2>{category}</h2>
-      <IconList icons={iconData[category]} />
-    </section>
-  )
-}
-
-const IconGallery = memo(function IconGallery() {
-  return (
-    <div className={styles.gallery}>
-      {Object.keys(iconData)
-        .sort()
-        .map((category) => (
-          <IconSection key={category} category={category} />
-        ))}
-    </div>
-  )
 })
 
-export default IconGallery
+export default IconList
