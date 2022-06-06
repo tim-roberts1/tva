2bd7a8fa47cd4a6b6a49f81a97c9adc3eaa47c13
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-06 10:58:12 -0400

    refactor(headless-styles): flatten single-use consts in Icon API

--- packages/headless-styles/src/components/Icon/shared.ts
+++ packages/headless-styles/src/components/Icon/shared.ts
@@ -1,12 +1,6 @@
 import type { IconOptions, IconA11yOptions, Tech } from './types'
 import { IconSize } from '../types'
 
-const a11yRole = 'img'
-const a11yPropMap = {
-  ariaHidden: 'aria-hidden',
-  ariaLabel: 'aria-label',
-}
-
 export const defaultIconOptions = {
   size: 'm' as IconSize,
   tech: '' as Tech,
@@ -24,10 +18,10 @@ export function getDefaultIconOptions(options?: IconOptions) {
 
 export function getA11yIconProps(a11yOptions?: IconA11yOptions) {
   return {
-    [a11yPropMap.ariaHidden]: a11yOptions?.ariaHidden,
+    'aria-hidden': a11yOptions?.ariaHidden,
     ...(a11yOptions?.ariaLabel && {
-      [a11yPropMap.ariaLabel]: a11yOptions.ariaLabel,
+      'aria-label': a11yOptions.ariaLabel,
     }),
-    role: a11yRole,
+    role: 'img',
   }
 }
