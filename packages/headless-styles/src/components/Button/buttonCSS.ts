4f9924178bdc03fb3a8901faa04c827e2ceecbb6
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-07 11:12:06 -0400

    refactor(headless-styles): simplify createButton args

--- packages/headless-styles/src/components/Button/buttonCSS.ts
+++ packages/headless-styles/src/components/Button/buttonCSS.ts
@@ -1,14 +1,11 @@
 import type { ClassOptions } from '../../utils/helpers'
 import { createClassProp } from '../../utils/helpers'
+import type { Tech } from '../types'
 import { getDefaultOptions, getDefaultDangerOptions } from './shared'
 import type { ButtonOptions, ButtonType, DangerOptions } from './types'
 import styles from './buttonCSS.module.css'
 
-type AllButtonOptions = ButtonOptions | DangerOptions
-
-function createButton(options: AllButtonOptions, classes: ClassOptions) {
-  const tech = options?.tech ?? ''
-
+function createButton(tech: Tech, classes: ClassOptions) {
   return {
     ...createClassProp(tech, classes),
     type: 'button' as ButtonType,
@@ -22,7 +19,7 @@ export function getDangerButtonProps(options?: DangerOptions) {
   const { kind, size } = defaultOptions
   const dangerKind = `${kind}Danger`
 
-  return createButton(defaultOptions, {
+  return createButton(defaultOptions.tech, {
     defaultClass: `ps-danger-btn ${styles[dangerKind]} ${styles[size]}`,
     svelteClass: `base ${kind}Danger ${size}`,
   })
@@ -32,7 +29,7 @@ export function getButtonProps(options?: ButtonOptions) {
   const defaultOptions = getDefaultOptions(options)
   const { kind, size } = defaultOptions
 
-  return createButton(defaultOptions, {
+  return createButton(defaultOptions.tech, {
     defaultClass: `ps-btn ${styles[kind]} ${styles[size]}`,
     svelteClass: `base ${kind} ${size}`,
   })
