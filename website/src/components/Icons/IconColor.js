deb1642650253b27d541dfe37c7c0f9ff6d60538
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-09 16:14:06 -0400

    docs(icons): improve icon color example

---
+++ website/src/components/Icons/IconColor.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { getIconProps } from '@pluralsight/headless-styles'
+import { WarningIcon } from '@pluralsight/icons'
+import Container from '../Container/Container'
+
+const psIconProps = getIconProps()
+
+function IconColor() {
+  return (
+    <Container>
+      <div
+        style={{
+          color: 'var(--ps-danger-text)',
+          backgroundColor: 'var(--ps-danger-background-weak)',
+          display: 'flex',
+          alignItems: 'center',
+          padding: '1em',
+          borderRadius: '3px',
+          width: '100%',
+        }}
+      >
+        <WarningIcon {...psIconProps} />
+        &nbsp; This is a warning!
+      </div>
+    </Container>
+  )
+}
+
+export default IconColor
--- website/src/components/Icons/IconColor.preview.js
+++ website/src/components/Icons/IconColor.preview.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import CodeBlock from '@theme/CodeBlock'
 
-const preview = `<div style={{
-  color: 'var(--ps-danger-text)'
-  background: 'var(--ps-danger-background)'
-}}>
-  ...
-  <WarningIcon />
-  ...
+const preview = `<div
+  style={{
+    color: 'var(--ps-danger-text)',
+    backgroundColor: 'var(--ps-danger-background-weak)',
+  }}
+>
+  <WarningIcon {...getIconProps} />
+  &nbsp; This is a warning!
 </div>`
 
 export function IconColorPreview() {
@@ -17,9 +18,25 @@ export function IconColorPreview() {
 export function IconColorFullPreview() {
   return (
     <CodeBlock>{`import { WarningIcon } from '@pluralsight/icons'
+import { getIconProps } from '@pluralsight/headless-styles'
 
-const DangerBlock = (
-${preview.replace(/^(.)/gm, '  $1')}
-)`}</CodeBlock>
+function Warning(props) {
+  return (
+    <div
+      style={{
+        color: 'var(--ps-danger-text)',
+        backgroundColor: 'var(--ps-danger-background-weak)',
+        display: 'flex',
+        alignItems: 'center',
+        padding: '1em',
+        borderRadius: '3px',
+        width: '100%',
+      }}
+    >
+      <WarningIcon {...getIconProps} />
+      &nbsp; {props.children}
+    </div>
+  )
+}`}</CodeBlock>
   )
 }
