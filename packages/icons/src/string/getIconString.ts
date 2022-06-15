134cb5eedd0a64da404d40ac3cbcc9314dc09217
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-15 11:37:40 -0400

    feat(icons): add and use css color type

--- packages/icons/src/string/getIconString.ts
+++ packages/icons/src/string/getIconString.ts
@@ -1,8 +1,12 @@
 import { iconStrings } from './generated/iconStrings'
+import type { CSSColor } from './types'
 
 export type IconName = keyof typeof iconStrings
 
-export default function getIconString(name: IconName, color = 'currentColor') {
+export default function getIconString<T extends string>(
+  name: IconName,
+  color: CSSColor<T> = 'currentColor'
+) {
   const dataPrefix = 'data:image/svg+xml;utf8,'
   return (
     dataPrefix +
