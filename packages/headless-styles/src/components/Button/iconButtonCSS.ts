54546ff29e0d573df9673f38b6b3dd1000a588e3
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-21 16:17:35 -0400

    refactor(headless-styles): getIconButtonprops - simplify use of size parameter

--- packages/headless-styles/src/components/Button/iconButtonCSS.ts
+++ packages/headless-styles/src/components/Button/iconButtonCSS.ts
@@ -5,9 +5,8 @@ import styles from './buttonCSS.module.css'
 
 export function getIconButtonProps(options?: IconButtonOptions) {
   const defaultOptions = getDefaultIconButtonOptions(options)
-  const { kind } = defaultOptions
+  const { kind, size } = defaultOptions
   const variantClass = `${defaultOptions.variant}IconButton`
-  const sizeClass = `${defaultOptions.size}`
   const props = getIconButtonReturnProps(defaultOptions)
 
   return {
@@ -15,8 +14,8 @@ export function getIconButtonProps(options?: IconButtonOptions) {
     button: {
       ...props.button,
       ...createClassProp(defaultOptions.tech, {
-        defaultClass: `ps-icon-btn ${styles[variantClass]} ${styles[kind]} ${styles[sizeClass]}`,
-        svelteClass: `base ${variantClass} ${kind} ${sizeClass}`,
+        defaultClass: `ps-icon-btn ${styles[variantClass]} ${styles[kind]} ${styles[size]}`,
+        svelteClass: `base ${variantClass} ${kind} ${size}`,
       }),
     },
   }
