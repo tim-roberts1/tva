41ebb67b7db95a9f82e950b2e7b9d96b25e85fbb
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-21 11:55:05 -0400

    docs(headless-styles): add danger icon button placeholder examples

---
+++ website/src/components/IconButton/DangerIconButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { getDangerIconButtonProps } from '@pluralsight/headless-styles'
+import Container from '../Container/Container'
+
+// const psButtonProps = getDangerButtonProps()
+// const psMediumButtonProps = getDangerButtonProps({ kind: 'medium' })
+// const psStrongButtonProps = getDangerButtonProps({ kind: 'strong' })
+
+function DangerIconButton() {
+  return (
+    <Container>
+      TBD
+      {/* <button {...psButtonProps}>default</button> */}
+      {/* <button {...psMediumButtonProps}>medium</button> */}
+      {/* <button {...psStrongButtonProps}>strong</button> */}
+    </Container>
+  )
+}
+
+export default DangerIconButton
---
+++ website/src/components/IconButton/DangerIconButton.preview.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CodeBlock from '@theme/CodeBlock'
+
+export function DangerIconButtonPreview() {
+  return <CodeBlock>{`TBD`}</CodeBlock>
+}
+
+export function DangerIconButtonFullPreview() {
+  return <CodeBlock>{`TBD`}</CodeBlock>
+}
