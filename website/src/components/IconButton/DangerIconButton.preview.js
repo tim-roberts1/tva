c6ced6458a724d9b09fe16aef6296278e3e89886
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-22 17:36:26 -0400

    docs(headless-styles): add danger icon button code example

--- website/src/components/IconButton/DangerIconButton.preview.js
+++ website/src/components/IconButton/DangerIconButton.preview.js
@@ -2,9 +2,35 @@ import React from 'react'
 import CodeBlock from '@theme/CodeBlock'
 
 export function DangerIconButtonPreview() {
-  return <CodeBlock>{`TBD`}</CodeBlock>
+  return (
+    <CodeBlock>{`const { button, iconOptions } = getDangerIconButtonProps({
+  ariaLabel: 'Delete',
+})
+
+<button {...button}>
+  <DeleteIcon {...getIconProps(iconOptions)} />
+</button>`}</CodeBlock>
+  )
 }
 
 export function DangerIconButtonFullPreview() {
-  return <CodeBlock>{`TBD`}</CodeBlock>
+  return (
+    <CodeBlock>{`import { getDangerIconButtonProps, getIconProps } from '@pluralsight/headless-styles'
+import { DeleteIcon } from '@pluralsight/icons'
+
+export default function DeleteButton(props) {
+  const { button, iconOptions } = getDangerIconButtonProps({
+    ariaLabel: props.label,
+    kind: props.kind,
+    size: props.size,
+    variant: props.variant,
+  })
+
+  return (
+    <button {...button} onClick={props.onClick}>
+      <MenuIcon {...getIconProps(iconOptions)} />
+    </button>
+  )
+}`}</CodeBlock>
+  )
 }
