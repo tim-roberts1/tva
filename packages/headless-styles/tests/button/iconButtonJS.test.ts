595108ef66828b8b9719063a22aba691cbdbaab3
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-22 11:25:08 -0400

    test(headless-styles): add tests for getJSDangerIconButtonProps

--- packages/headless-styles/tests/button/iconButtonJS.test.ts
+++ packages/headless-styles/tests/button/iconButtonJS.test.ts
@@ -1,138 +1,267 @@
-import { getJSIconButtonProps } from '../../src'
+import { getJSDangerIconButtonProps, getJSIconButtonProps } from '../../src'
 
-describe('getJSIconButtonProps', () => {
-  const baseOptions = {
-    ariaLabel: 'JS Icon Button',
-  }
+describe('JS Icon Button', () => {
+  describe('getJSIconButtonProps', () => {
+    const baseOptions = {
+      ariaLabel: 'JS Icon Button',
+    }
 
-  test('should accept an ariaLabel prop', () => {
-    const iconButtonReturn = getJSIconButtonProps(baseOptions)
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-    expect(iconButtonReturn.button['aria-label']).toEqual('JS Icon Button')
-  })
-
-  test('should accept a textWeak kind type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      kind: 'textWeak',
+    test('should accept an ariaLabel prop', () => {
+      const iconButtonReturn = getJSIconButtonProps(baseOptions)
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+      expect(iconButtonReturn.button['aria-label']).toEqual('JS Icon Button')
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a textWeak kind type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        kind: 'textWeak',
+      })
 
-  test('should accept a weak kind type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      kind: 'weak',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a weak kind type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        kind: 'weak',
+      })
 
-  test('should accept a medium kind type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      kind: 'medium',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a medium kind type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        kind: 'medium',
+      })
 
-  test('should accept a strong kind type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      kind: 'strong',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a strong kind type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        kind: 'strong',
+      })
 
-  test('should accept a xs size type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      size: 'xs',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a xs size type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        size: 'xs',
+      })
 
-  test('should accept a small size type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      size: 's',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a small size type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        size: 's',
+      })
 
-  test('should accept a medium size type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      size: 'm',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a medium size type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        size: 'm',
+      })
 
-  test('should accept a large size type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      size: 'l',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a large size type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        size: 'l',
+      })
 
-  test('should accept a blank variant type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
-  })
+    test('should accept a blank variant type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+      })
 
-  test('should accept a round variant type', () => {
-    const iconButtonReturn = getJSIconButtonProps({
-      ...baseOptions,
-      variant: 'round',
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
     })
 
-    expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
-    expect(iconButtonReturn.button.styles).toBeDefined()
-    expect(iconButtonReturn.button.type).toEqual('button')
-    expect(iconButtonReturn.iconOptions).toBeDefined()
+    test('should accept a round variant type', () => {
+      const iconButtonReturn = getJSIconButtonProps({
+        ...baseOptions,
+        variant: 'round',
+      })
 
-    expect(iconButtonReturn.button.cssProps).toContain('border-radius: 50%')
-    expect(iconButtonReturn.button.styles.borderRadius).toEqual('50%')
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+
+      expect(iconButtonReturn.button.cssProps).toContain('border-radius: 50%')
+      expect(iconButtonReturn.button.styles.borderRadius).toEqual('50%')
+    })
+  })
+
+  describe('getJSDangerIconButtonProps', () => {
+    const baseOptions = {
+      ariaLabel: 'JS Danger Icon Button',
+    }
+
+    test('should accept an ariaLabel prop', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps(baseOptions)
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+      expect(iconButtonReturn.button['aria-label']).toEqual(
+        'JS Danger Icon Button'
+      )
+    })
+
+    test('should accept a text kind type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        kind: 'text',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a medium kind type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        kind: 'medium',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a strong kind type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        kind: 'strong',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a xs size type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        size: 'xs',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a small size type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        size: 's',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a medium size type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        size: 'm',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a large size type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        size: 'l',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a blank variant type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+    })
+
+    test('should accept a round variant type', () => {
+      const iconButtonReturn = getJSDangerIconButtonProps({
+        ...baseOptions,
+        variant: 'round',
+      })
+
+      expect(iconButtonReturn.button.cssProps).not.toHaveLength(0)
+      expect(iconButtonReturn.button.styles).toBeDefined()
+      expect(iconButtonReturn.button.type).toEqual('button')
+      expect(iconButtonReturn.iconOptions).toBeDefined()
+
+      expect(iconButtonReturn.button.cssProps).toContain('border-radius: 50%')
+      expect(iconButtonReturn.button.styles.borderRadius).toEqual('50%')
+    })
   })
 })
