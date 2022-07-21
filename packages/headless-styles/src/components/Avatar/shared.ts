b38f925486864b52d9d59c55355eef2df85db2a1
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-07-21 09:39:39 -0400

    feat(headless-styles): add shared Avatar API methods

---
+++ packages/headless-styles/src/components/Avatar/shared.ts
@@ -0,0 +1,33 @@
+import type { Tech } from '../types'
+import type { AvatarOptions, Size, Kind } from './types'
+
+export const defaultAvatarOptions = {
+  ariaLabel: '',
+  kind: 'neutral' as Kind,
+  size: 'm' as Size,
+  tech: '' as Tech,
+}
+
+export const iconSizeMap: Record<Size, string> = {
+  xs: '1.5rem',
+  s: '2.5rem',
+  m: '4rem',
+  l: '6rem',
+  xl: '8rem',
+}
+
+export function getDefaultAvatarOptions(options?: AvatarOptions) {
+  return {
+    ariaLabel: options?.ariaLabel ?? defaultAvatarOptions.ariaLabel,
+    kind: options?.kind ?? defaultAvatarOptions.kind,
+    size: options?.size ?? defaultAvatarOptions.size,
+    tech: options?.tech ?? defaultAvatarOptions.tech,
+  }
+}
+
+export function createAvatarSelectorClasses(kind: Kind, size: Size) {
+  return {
+    kindClass: `${kind}Avatar`,
+    sizeClass: `${size}Avatar`,
+  }
+}
