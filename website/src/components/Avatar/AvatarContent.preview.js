cb51bebc7f85dca526c05f2139b3ed2b95d35082
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-07-26 10:17:24 -0400

    docs(headless-styles): add avatar content code examples

---
+++ website/src/components/Avatar/AvatarContent.preview.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import CodeBlock from '@theme/CodeBlock'
+
+export function AvatarContentPreview() {
+  return (
+    <CodeBlock>{`const { avatar, image, iconOptions } = getAvatarProps({
+  ariaLabel: "Weird Al's Avatar"
+})
+
+<span {...avatar}><img {...image} src={weirdAlSrc} alt="Weird Al" /></span>
+<span {...avatar}>WA</span>
+<span {...avatar}><ProfileIcon {...getIconProps(iconOptions)} /></span>
+`}</CodeBlock>
+  )
+}
+
+export function AvatarContentFullPreview() {
+  return (
+    <CodeBlock>{`import { getAvatarProps, getIconProps } from '@pluralsight/headless-styles'
+import { PersonIcon } from '@pluralsight/icons'
+
+function FallbackAvatar(props) {
+  const { avatar, iconOptions } = getAvatarProps(props)
+
+  return (
+    <span {...avatar}><PersonIcon {...iconProps} /></span>
+  )
+}
+
+function InitialAvatar({ initials, ...avatarProps }) {
+  const { avatar } = getAvatarProps(avatarProps)
+
+  return (
+    <span {...avatar}>{initials}</span>
+  )
+}
+
+function ImageAvatar({ src, name, ...avatarProps }) {
+  const { avatar, image } = getAvatarProps(avatarProps)
+
+  return (
+    <span {...avatar}><img src={src} alt={name} {...image} /></span>
+  )
+}
+
+export default function Avatar(props) {
+  if (props.src) {
+    return <ImageAvatar {...props} />
+  } else if (props.initials) {
+    return <InitialAvatar {...props} />
+  }
+
+  return <FallbackAvatar {...props} />
+}`}</CodeBlock>
+  )
+}
