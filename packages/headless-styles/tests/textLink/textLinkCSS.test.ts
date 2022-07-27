561af0c350c73a34cbdf9a39d55adb074ae0272c
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-07-27 09:59:58 -0400

    test(headless-styles): add tests for CSS Text Link API

---
+++ packages/headless-styles/tests/textLink/textLinkCSS.test.ts
@@ -0,0 +1,26 @@
+import { getTextLinkProps } from '../../src'
+
+describe('Text Link CSS - getTextLinkProps', () => {
+  const baseClass = 'ps-text-link'
+  const defaultResult = {
+    link: {
+      className: `${baseClass} baseTextLink`,
+    },
+    iconOptions: {
+      customSize: '1em',
+    },
+  }
+
+  test('should allow no props to be passed in', () => {
+    expect(getTextLinkProps()).toEqual(defaultResult)
+  })
+
+  test('should accept a tech type', () => {
+    expect(getTextLinkProps({ tech: 'svelte' })).toEqual({
+      ...defaultResult,
+      link: {
+        class: 'baseTextLink',
+      },
+    })
+  })
+})
