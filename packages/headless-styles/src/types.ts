d2b1a6cb52dada24f91e74052ac3f37b2477096f
Casey Baggz <caseybaggz@gmail.com>
2022-07-27 13:23:19 -0500

    chore(headless): revert types.ts

--- packages/headless-styles/src/types.ts
+++ packages/headless-styles/src/types.ts
@@ -1,26 +1,23 @@
-// eslint-disable-next-line
-// @ts-nocheck
-
-export { AlertOptions } from './components/Alert/types'
-export { AlertDialogOptions } from './components/AlertDialog/types'
-export { AvatarOptions } from './components/Avatar/types'
-export { BadgeOptions } from './components/Badge/types'
-export {
+export type { AlertOptions } from './components/Alert/types'
+export type { AlertDialogOptions } from './components/AlertDialog/types'
+export type { AvatarOptions } from './components/Avatar/types'
+export type { BadgeOptions } from './components/Badge/types'
+export type {
   ButtonOptions,
   IconButtonOptions,
   DangerIconButtonOptions,
 } from './components/Button/types'
-export { CheckboxOptions } from './components/Checkbox/types'
-export { CircularProgressOptions } from './components/CircularProgress/types'
-export { ErrorMessageOptions } from './components/ErrorMessage/types'
-export { FieldMessageOptions } from './components/FieldMessage/types'
-export { FormControlOptions } from './components/FormControl/types'
-export { FormLabelOptions } from './components/FormLabel/types'
-export { IconOptions } from './components/Icon/types'
-export { InputOptions } from './components/Input/types'
-export { ProgressOptions } from './components/Progress/types'
-export { RadioOptions } from './components/Radio/types'
-export { SkeletonOptions } from './components/Skeleton/types'
-export { SwitchOptions } from './components/Switch/types'
-export { TagOptions } from './components/Tag/types'
-export { TextareaOptions } from './components/Textarea/types'
+export type { CheckboxOptions } from './components/Checkbox/types'
+export type { CircularProgressOptions } from './components/CircularProgress/types'
+export type { ErrorMessageOptions } from './components/ErrorMessage/types'
+export type { FieldMessageOptions } from './components/FieldMessage/types'
+export type { FormControlOptions } from './components/FormControl/types'
+export type { FormLabelOptions } from './components/FormLabel/types'
+export type { IconOptions } from './components/Icon/types'
+export type { InputOptions } from './components/Input/types'
+export type { ProgressOptions } from './components/Progress/types'
+export type { RadioOptions } from './components/Radio/types'
+export type { SkeletonOptions } from './components/Skeleton/types'
+export type { SwitchOptions } from './components/Switch/types'
+export type { TagOptions } from './components/Tag/types'
+export type { TextareaOptions } from './components/Textarea/types'
