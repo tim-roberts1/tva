d7351abc3568fa71c25000fbe80810e411f8886c
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-08-23 15:19:16 -0400

    docs(headless-styles): update basic badge examples

--- website/src/components/Badge/BasicBadge.js
+++ website/src/components/Badge/BasicBadge.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Container from '../Container/Container'
-import { getBadgeProps } from '@pluralsight/headless-styles'
-
-const psBadgeProps = getBadgeProps()
-const psStrongBadgeProps = getBadgeProps({ kind: 'strong' })
-const psMediumBadgeProps = getBadgeProps({ kind: 'medium' })
-const psWeakBadgeProps = getBadgeProps({ kind: 'weak' })
+import Badge from './Badge'
 
 export default function BasicBadge() {
   return (
     <Container>
-      <span {...psBadgeProps}>Default</span>
-      <span {...psStrongBadgeProps}>Strong</span>
-      <span {...psMediumBadgeProps}>Medium</span>
-      <span {...psWeakBadgeProps}>Weak</span>
+      <Badge>Default</Badge>
+      <Badge sentiment="action">Action</Badge>
     </Container>
   )
 }
--- website/src/components/Badge/BasicBadge.preview.js
+++ website/src/components/Badge/BasicBadge.preview.js
@@ -3,24 +3,20 @@ import CodeBlock from '@theme/CodeBlock'
 
 export function BasicBadgePreview() {
   return (
-    <CodeBlock>{`<span {...getBadgeProps()}>Default</span>
-<span {...getBadgeProps({ kind: 'strong' })}>Strong</span>
-<span {...getBadgeProps({ kind: 'medium' })}>Medium</span>
-<span {...getBadgeProps({ kind: 'weak' })}>Weak</span>`}</CodeBlock>
+    <CodeBlock>{`<span {...getBadgeProps().badge}>Default</span>
+<span {...getBadgeProps({ sentiment: 'action' }).badge}>Action</span>`}</CodeBlock>
   )
 }
 
 export function BasicBadgeFullPreview() {
   return (
-    <CodeBlock>{`import { getBadgeProps } from '@pluralsight/headless-styles';
+    <CodeBlock>{`import { getBadgeProps } from '@pluralsight/headless-styles'
 
 export default function BasicBadges() {
   return (
     <div>
-      <span {...getBadgeProps()}>Default</span>
-      <span {...getBadgeProps({ kind: 'strong' })}>Strong</span>
-      <span {...getBadgeProps({ kind: 'medium' })}>Medium</span>
-      <span {...getBadgeProps({ kind: 'weak' })}>Weak</span>
+      <span {...getBadgeProps().badge}>Default</span>
+      <span {...getBadgeProps({ sentiment: 'action' }).badge}>Action</span>
     </div>
   );
 }`}</CodeBlock>
