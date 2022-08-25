2da1d8809e7bea2e49a84c645a946235e982e24b
Casey Baggz <caseybaggz@gmail.com>
2022-08-25 10:17:40 -0500

    fix(headless): update chakraAlertDialog styles

--- packages/headless-styles/src/components/ConfirmDialog/chakraAlertDialog.ts
+++ packages/headless-styles/src/components/ConfirmDialog/chakraAlertDialog.ts
@@ -3,6 +3,13 @@ import styles from './generated/confirmDialogCSS.module'
 const modalParts = ['overlay', 'dialogContainer', 'dialog']
 const modalExtend = ['header', 'closeButton', 'body', 'footer']
 
+const chakraAlertReset = {
+  paddingBottom: 0,
+  paddingInlineEnd: '1.5rem',
+  paddingInlineStart: '1.5rem',
+  paddingTop: 0,
+}
+
 export const ChakraAlertDialog = {
   parts: [...modalParts, ...modalExtend],
   baseStyle: {
@@ -12,8 +19,22 @@ export const ChakraAlertDialog = {
     dialog: {
       background: styles.confirmDialogSection.background,
     },
-    header: styles.confirmDialogTitle,
-    body: {},
+    header: {
+      ...styles.confirmDialogTitle,
+      ...chakraAlertReset,
+      fontSize: '1.125rem',
+      fontWeight: '700',
+    },
+    body: {
+      ...chakraAlertReset,
+      fontSize: '1rem',
+      fontWeight: '500',
+    },
+    footer: {
+      ...chakraAlertReset,
+      marginBottom: '1.5rem',
+      marginTop: '1.5rem',
+    },
   },
   sizes: {},
   defaultProps: {
