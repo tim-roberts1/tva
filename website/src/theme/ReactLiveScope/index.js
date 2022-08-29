ae75927777fe26844a9f392a197423d8d4e6c38b
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-08-29 10:08:06 -0400

    docs(headless-styles): add getIconProps and PlaceholderIcon to live examples scope

---
+++ website/src/theme/ReactLiveScope/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { getIconProps } from '@pluralsight/headless-styles'
+import { PlaceholderIcon } from '@pluralsight/icons'
+
+const ReactLiveScope = {
+  React,
+  ...React,
+  getIconProps,
+  PlaceholderIcon,
+}
+
+export default ReactLiveScope
