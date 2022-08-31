176dbe0acee6254ea60de7d784f3f72838f92ec0
Casey Baggz <caseybaggz@gmail.com>
2022-08-31 14:00:05 -0500

    chore: remove unused var in vite.config.js

--- packages/headless-styles/sandbox/vite.config.js
+++ packages/headless-styles/sandbox/vite.config.js
@@ -5,7 +5,7 @@ import transformCSStoTS from './plugins/transformCSStoTS'
 const cssRegex = /\/components\/.*\.css$/
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
