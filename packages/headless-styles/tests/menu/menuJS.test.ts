7d96b46e6560784d48d0f72a227557e609c3a236
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-09-06 14:38:47 -0400

    test(headless-styles): add unit tests for menu JS API

---
+++ packages/headless-styles/tests/menu/menuJS.test.ts
@@ -0,0 +1,33 @@
+import { UNSAFE_getJSMenuProps as getJSMenuProps } from '../../src'
+
+describe('Menu CSS - getJSMenuProps', () => {
+  test('should accept a label option', () => {
+    const menuProps = getJSMenuProps({ label: 'submenu' })
+
+    expect(menuProps.menu.a11yProps['aria-label']).toEqual('submenu')
+  })
+
+  test('should accept an isSubmenu option', () => {
+    const menuProps = getJSMenuProps({ isSubmenu: true })
+
+    expect(menuProps.menuItem.a11yProps['aria-haspopup']).toEqual(true)
+    expect(menuProps.menuItem.cssProps).toContain('display: flex')
+    expect(menuProps.menuItem.styles?.display).toEqual('flex')
+    expect(menuProps.menu.cssProps).toContain('display: none')
+    expect(menuProps.menu.styles?.display).toEqual('none')
+  })
+
+  test('should accept an isSubmenuExpanded option', () => {
+    const menuProps = getJSMenuProps({ isSubmenuExpanded: true })
+
+    expect(menuProps.menuItem.a11yProps['aria-expanded']).toEqual(true)
+    expect(menuProps.menuItem.cssProps).toContain(
+      'background: var(--ps-action-background)'
+    )
+    expect(menuProps.menuItem.styles?.background).toEqual(
+      'var(--ps-action-background)'
+    )
+    expect(menuProps.menu.cssProps).toContain('display: block')
+    expect(menuProps.menu.styles?.display).toEqual('block')
+  })
+})
