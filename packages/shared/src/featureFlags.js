3de471f0cd67661bb03bccd454def71f12ae4919
Casey Baggz <caseybaggz@gmail.com>
2022-09-12 09:43:48 -0500

    ci: update next to update hs shared dep

--- packages/shared/src/featureFlags.js
+++ packages/shared/src/featureFlags.js
@@ -1,22 +1,21 @@
 const __EXPERIMENTAL__ = process.env.RELEASE_CHANNEL === 'experimental' ?? false
 // const __NEXT__ = process.env.RELEASE_CHANNEL === 'next' ?? false
-// const __LATEST__ = process.env.RELEASE_CHANNEL === 'latest' ?? false
 
 // -----------------------------------------------------------------------------
-// Land or remove (zero effort)
+// Land or remove (zero effort) - already released - should remove flag logic
 //
 // Flags that can likely be deleted or landed without consequences
 // -----------------------------------------------------------------------------
 
 // -----------------------------------------------------------------------------
-// Land or remove (moderate effort)
+// Land or remove (moderate effort) - next release
 //
 // Flags that can be probably deleted or landed, but might require extra effort
 // like migrating internal callers or performance testing.
 // -----------------------------------------------------------------------------
 
 // -----------------------------------------------------------------------------
-// Ongoing experiments
+// Ongoing experiments - experimental release
 //
 // These are features that we're either actively exploring or are reasonably
 // likely to include in an upcoming release.
