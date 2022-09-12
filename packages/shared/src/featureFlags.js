b6e61b87a96e8f16ee3c24bc76edb80d89a7c00d
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-09-12 14:35:01 -0400

    feat(headless-styles): move menu API to 'next' release channel

--- packages/shared/src/featureFlags.js
+++ packages/shared/src/featureFlags.js
@@ -1,5 +1,5 @@
-const __EXPERIMENTAL__ = process.env.RELEASE_CHANNEL === 'experimental' ?? false
-// const __NEXT__ = process.env.RELEASE_CHANNEL === 'next' ?? false
+// const __EXPERIMENTAL__ = process.env.RELEASE_CHANNEL === 'experimental' ?? false
+const __NEXT__ = process.env.RELEASE_CHANNEL === 'next' ?? false
 
 // -----------------------------------------------------------------------------
 // Land or remove (zero effort) - ready for public - should remove flag logic
@@ -13,6 +13,7 @@ const __EXPERIMENTAL__ = process.env.RELEASE_CHANNEL === 'experimental' ?? false
 // Flags that can be probably deleted or landed, but might require extra effort
 // like migrating internal callers or performance testing.
 // -----------------------------------------------------------------------------
+export const menu = __NEXT__
 
 // -----------------------------------------------------------------------------
 // Ongoing experiments - experimental release
@@ -20,5 +21,3 @@ const __EXPERIMENTAL__ = process.env.RELEASE_CHANNEL === 'experimental' ?? false
 // These are features that we're either actively exploring or are reasonably
 // likely to include in an upcoming release.
 // -----------------------------------------------------------------------------
-
-export const menu = __EXPERIMENTAL__
