0a4294402d3f4a2814014eabcb4ba3cfc0a28e9f
Tom Pietrosanti <Rykus0@users.noreply.github.com>
2022-09-14 12:05:09 -0400

    style(headless-style): remove useless package rename
    Co-authored-by: Casey Baggz <caseybaggz@gmail.com>

--- packages/headless-styles/tests/menu/menuJS.test.ts
+++ packages/headless-styles/tests/menu/menuJS.test.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck still figuring out env variables
 
-import { getJSMenuProps as getJSMenuProps } from '../../src'
+import { getJSMenuProps } from '../../src'
 
 describe('Menu CSS - getJSMenuProps', () => {
   test('should accept a label option', () => {
