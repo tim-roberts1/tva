44eb0d4a5db071b3dd8b0dcf744e26988694499e
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-09-16 16:12:42 -0400

    feat(design-tokens): add tokens to experimental releases for this update

--- versions.mjs
+++ versions.mjs
@@ -40,4 +40,9 @@ export const stablePackages = {
 // These packages do not exist in the @next or @latest channel, only
 // @experimental. We don't use semver, just the commit sha, so this is just a
 // list of package names instead of a map.
-export const experimentalPackages = ['shared', 'headless-styles', 'react-utils']
+export const experimentalPackages = [
+  'shared',
+  'headless-styles',
+  'react-utils',
+  'design-tokens',
+]
