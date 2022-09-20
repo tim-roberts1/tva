6e2c3e4b9625a56a0dcdb04406dcd96028a07d3d
Casey Baggz <caseybaggz@gmail.com>
2022-09-20 10:58:02 -0500

    ci(shared): update processFlags to remove netlify if in GH CI

--- packages/shared/scripts/processFlags.mjs
+++ packages/shared/scripts/processFlags.mjs
@@ -5,25 +5,47 @@ import { info, error } from './theme.mjs'
 
 async function run() {
   const channel = process.env.RELEASE_CHANNEL ?? ''
+  const ghAction = process.env.GITHUB_ACTION ?? ''
+  const featureFlagFile = join(getWorkspacePath(), 'utils/helpers.ts')
 
   warning(
     channel,
     `Processing feature flags requires a RELEASE_CHANNEL env var to be set.`
   )
 
-  const featureFlagFile = join(getWorkspacePath(), 'utils/helpers.ts')
-
   console.log(info('\n📝  Updating feature flags for channel: ' + channel))
+  await updateReleaseChannel(featureFlagFile, channel)
 
+  if (ghAction) {
+    console.log(info('\n📝  Updating Netlify logic for package.'))
+    await updateNetlifyVars(featureFlagFile)
+  }
+}
+
+async function updateReleaseChannel(filePath, channel) {
   try {
     await replace({
-      files: featureFlagFile,
+      files: filePath,
       from: /process\.env\.RELEASE_CHANNEL/g,
       to: `'${channel}'`,
     })
-    console.log(info('Feature flag file contents updated.'))
+    console.log(info('featureFlags.js contents updated with channel value.'))
   } catch (err) {
     console.log(error('Unable to update featureFlags.js with channel value.'))
+    console.log(error(err))
+  }
+}
+
+async function updateNetlifyVars(filePath) {
+  try {
+    await replace({
+      files: filePath,
+      from: [/process\.env\.NETLIFY/g, /process\.env\.CONTEXT/g],
+      to: `'${false}'`,
+    })
+    console.log(info('featureFlags.js contents updated with netlify values.'))
+  } catch (err) {
+    console.log(error('Unable to update featureFlags.js with netlify values.'))
     console.log(error(err))
   }
 }
