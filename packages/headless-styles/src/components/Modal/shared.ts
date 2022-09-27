8411dee5920c3c134c69e2beed7f6ba01b492638
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-09-27 15:34:59 -0400

    feat(headless-styles): add prop helper for modal

--- packages/headless-styles/src/components/Modal/shared.ts
+++ packages/headless-styles/src/components/Modal/shared.ts
@@ -1,4 +1,5 @@
 import type { Tech } from '../types'
+import type { IconButtonOptions } from '../IconButton/types'
 import type { ModalOptions } from './types'
 
 const defaultModalOptions = {
@@ -7,6 +8,20 @@ const defaultModalOptions = {
   headingId: '',
   id: '',
   tech: '' as Tech,
+}
+
+function getModalLabelProps(headingId?: string, ariaLabel?: string) {
+  // TODO: Throw a warning if both are missing
+
+  if (headingId) {
+    return {
+      'aria-labelledby': headingId,
+    }
+  }
+
+  return {
+    'aria-label': ariaLabel,
+  }
 }
 
 // Public
@@ -18,5 +33,43 @@ export function getDefaultModalOptions(options?: ModalOptions) {
     headingId: options?.headingId ?? defaultModalOptions.headingId,
     id: options?.id ?? defaultModalOptions.id,
     tech: options?.tech ?? defaultModalOptions.tech,
+  }
+}
+export function createModalProps(options: ModalOptions) {
+  const { bodyId, headingId } = options
+
+  return {
+    backdrop: {},
+    cancelBtnOptions: {
+      ariaLabel: 'Close dialog',
+      sentiment: 'default',
+      usage: 'text',
+      size: 'l',
+      tech: options.tech,
+    } as IconButtonOptions,
+    buttonWrapper: {},
+    focusGuard: {
+      'aria-hidden': true,
+      'data-focus-guard': true,
+      tabIndex: 0,
+    },
+    modalHeading: {
+      id: headingId,
+    },
+    modalBody: {
+      id: bodyId,
+    },
+    section: {
+      'aria-modal': true,
+      'aria-describedby': bodyId,
+      ...getModalLabelProps(headingId, options.ariaLabel),
+      id: options.id,
+      role: 'dialog',
+      tabIndex: -1,
+    },
+    wrapper: {
+      'data-focus-lock-disabled': false,
+      tabIndex: -1,
+    },
   }
 }
