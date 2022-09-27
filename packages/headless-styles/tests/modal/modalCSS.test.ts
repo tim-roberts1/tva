8eb0452f156baa782637b0efdbb0f4503454a860
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-09-27 15:25:39 -0400

    test(headless-styles): add tests for getModalProps

---
+++ packages/headless-styles/tests/modal/modalCSS.test.ts
@@ -0,0 +1,152 @@
+import { unstable_getModalProps } from '../../src'
+
+describe('Modal CSS', () => {
+  const baseClass = 'ps-modal'
+  const result = {
+    cancelBtnOptions: {
+      ariaLabel: 'Close dialog',
+      sentiment: 'default',
+      usage: 'text',
+      size: 'l',
+      tech: '',
+    },
+    backdrop: {
+      className: `${baseClass}-backdrop confirmDialogBackdrop`,
+    },
+    buttonWrapper: {
+      className: `${baseClass}-btn-wrapper modalButtonWrapper`,
+    },
+    focusGuard: {
+      'aria-hidden': true,
+      'data-focus-guard': true,
+      tabIndex: 0,
+      className: `${baseClass}-focus-guard confirmFocusGuard`,
+    },
+    modalHeading: {
+      id: '',
+      className: `${baseClass}-heading confirmDialogTitle modalHeading`,
+    },
+    modalBody: {
+      id: '',
+      className: `${baseClass}-body modalBody`,
+    },
+    section: {
+      'aria-modal': true,
+      'aria-describedby': '',
+      id: '',
+      role: 'dialog',
+      tabIndex: -1,
+      className: `${baseClass}-section confirmDialogSection modalSection`,
+    },
+    wrapper: {
+      'data-focus-lock-disabled': false,
+      tabIndex: -1,
+      className: `${baseClass} confirmDialogWrapper`,
+    },
+  }
+
+  test('should accept a heading ID', () => {
+    const bodyId = 'modal-body'
+    const headingId = 'modal-heading'
+
+    expect(
+      unstable_getModalProps({
+        id: 'modal-id',
+        headingId: headingId,
+        bodyId: bodyId,
+      })
+    ).toEqual({
+      ...result,
+      modalHeading: {
+        ...result.modalHeading,
+        id: headingId,
+      },
+      modalBody: {
+        ...result.modalBody,
+        id: bodyId,
+      },
+      section: {
+        ...result.section,
+        'aria-describedby': bodyId,
+        'aria-labelledby': headingId,
+        id: 'modal-id',
+      },
+    })
+  })
+
+  test('should accept an ariaLabel instead of headingId', () => {
+    const bodyId = 'modal-body'
+    const label = 'Heading for modal'
+
+    expect(
+      unstable_getModalProps({
+        id: 'modal-id',
+        ariaLabel: label,
+        bodyId: bodyId,
+      })
+    ).toEqual({
+      ...result,
+      modalBody: {
+        ...result.modalBody,
+        id: bodyId,
+      },
+      section: {
+        ...result.section,
+        'aria-describedby': bodyId,
+        'aria-label': label,
+        id: 'modal-id',
+      },
+    })
+  })
+
+  test('should accept a tech type', () => {
+    expect(
+      unstable_getModalProps({
+        id: 'id',
+        headingId: 'headingId',
+        bodyId: 'bodyId',
+        tech: 'svelte',
+      })
+    ).toEqual({
+      ...result,
+      cancelBtnOptions: {
+        ...result.cancelBtnOptions,
+        tech: 'svelte',
+      },
+      backdrop: {
+        class: `${baseClass}-backdrop confirmDialogBackdrop`,
+      },
+      buttonWrapper: {
+        class: `${baseClass}-btn-wrapper modalButtonWrapper`,
+      },
+      focusGuard: {
+        'aria-hidden': true,
+        'data-focus-guard': true,
+        tabIndex: 0,
+        class: `${baseClass}-focus-guard confirmFocusGuard`,
+      },
+      modalHeading: {
+        id: 'headingId',
+        class: `${baseClass}-heading confirmDialogTitle modalHeading`,
+      },
+      modalBody: {
+        id: 'bodyId',
+        class: `${baseClass}-body modalBody`,
+      },
+      section: {
+        'aria-modal': true,
+        'aria-describedby': 'bodyId',
+        'aria-labelledby': 'headingId',
+        id: 'id',
+        role: 'dialog',
+        tabIndex: -1,
+        class: `${baseClass}-section confirmDialogSection modalSection`,
+      },
+      wrapper: {
+        'data-focus-lock-disabled': false,
+        tabIndex: -1,
+        class: `${baseClass} confirmDialogWrapper`,
+      },
+    })
+  })
+})
