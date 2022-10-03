e9a1af021e6452c09f24e9ad8674274229bf5543
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-10-03 16:18:56 -0400

    docs(headless-styles): add Modal component for docs examples

---
+++ website/src/components/Modal/Modal.js
@@ -0,0 +1,56 @@
+import React, { forwardRef, memo, useEffect, useRef } from 'react'
+import { useEscToClose, useFocusTrap } from '@pluralsight/react-utils'
+import {
+  getIconButtonProps,
+  getIconProps,
+  unstable_getModalProps,
+} from '@pluralsight/headless-styles'
+import { CloseIcon } from '@pluralsight/icons'
+
+function ModalEl(props, triggerRef) {
+  const { onClose, ...modalProps } = props
+  const modal = unstable_getModalProps(modalProps)
+  console.log(modal)
+  const { button, iconOptions } = getIconButtonProps(modal.cancelBtnOptions)
+  const wrapperRef = useRef(null)
+  const { ref, onKeyDown, setupFocusTrap } = useFocusTrap(triggerRef)
+
+  function handleBackdropClick(event) {
+    event.stopPropagation()
+    if (wrapperRef.current === event.target) {
+      onClose()
+    }
+  }
+
+  useEscToClose(onClose)
+
+  useEffect(() => {
+    setupFocusTrap()
+  }, [setupFocusTrap])
+
+  return (
+    <div {...modal.backdrop}>
+      <div {...modal.focusGuard} />
+
+      <div {...modal.wrapper} ref={wrapperRef} onClick={handleBackdropClick}>
+        <section {...modal.section} ref={ref} onKeyDown={onKeyDown}>
+          <header>
+            <h6 {...modal.modalHeading}>{props.heading}</h6>
+          </header>
+          <p {...modal.modalBody}>{props.body}</p>
+          <footer {...modal.buttonWrapper}>
+            <button {...button}>
+              <CloseIcon {...getIconProps(iconOptions)} onClick={onClose} />
+            </button>
+          </footer>
+        </section>
+      </div>
+
+      <div {...modal.focusGuard} />
+    </div>
+  )
+}
+
+const Modal = memo(forwardRef(ModalEl))
+
+export default Modal
