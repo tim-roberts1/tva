6d9ad270a1b277bbd7c289ccc1735989b91f00ba
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-10-11 16:19:24 -0400

    feat(headless-styles): merge positioning ::after styles for tooltip JS

--- packages/headless-styles/src/components/Tooltip/tooltipJS.ts
+++ packages/headless-styles/src/components/Tooltip/tooltipJS.ts
@@ -10,61 +10,109 @@ function getTooltipPositionStyles(position: Position) {
       return {
         ...positionStyles.topPosition,
         ...positionStyles.horizontalStart,
+        '&::after': {
+          ...positionStyles.topPosition['&::after'],
+          ...positionStyles.horizontalStart['&::after'],
+        },
       }
     case 'top':
       return {
         ...positionStyles.topPosition,
         ...positionStyles.horizontalCenter,
+        '&::after': {
+          ...positionStyles.topPosition['&::after'],
+          ...positionStyles.horizontalCenter['&::after'],
+        },
       }
     case 'topEnd':
       return {
         ...positionStyles.topPosition,
         ...positionStyles.horizontalEnd,
+        '&::after': {
+          ...positionStyles.topPosition['&::after'],
+          ...positionStyles.horizontalEnd['&::after'],
+        },
       }
     case 'rightStart':
       return {
         ...positionStyles.rightPosition,
         ...positionStyles.verticalStart,
+        '&::after': {
+          ...positionStyles.rightPosition['&::after'],
+          ...positionStyles.verticalStart['&::after'],
+        },
       }
     case 'right':
       return {
         ...positionStyles.rightPosition,
         ...positionStyles.verticalCenter,
+        '&::after': {
+          ...positionStyles.rightPosition['&::after'],
+          ...positionStyles.verticalCenter['&::after'],
+        },
       }
     case 'rightEnd':
       return {
         ...positionStyles.rightPosition,
         ...positionStyles.verticalEnd,
+        '&::after': {
+          ...positionStyles.rightPosition['&::after'],
+          ...positionStyles.verticalEnd['&::after'],
+        },
       }
     case 'bottomStart':
       return {
         ...positionStyles.bottomPosition,
         ...positionStyles.horizontalStart,
+        '&::after': {
+          ...positionStyles.bottomPosition['&::after'],
+          ...positionStyles.horizontalStart['&::after'],
+        },
       }
     case 'bottom':
       return {
         ...positionStyles.bottomPosition,
         ...positionStyles.horizontalCenter,
+        '&::after': {
+          ...positionStyles.bottomPosition['&::after'],
+          ...positionStyles.horizontalCenter['&::after'],
+        },
       }
     case 'bottomEnd':
       return {
         ...positionStyles.bottomPosition,
         ...positionStyles.horizontalEnd,
+        '&::after': {
+          ...positionStyles.bottomPosition['&::after'],
+          ...positionStyles.horizontalEnd['&::after'],
+        },
       }
     case 'leftStart':
       return {
         ...positionStyles.leftPosition,
         ...positionStyles.verticalStart,
+        '&::after': {
+          ...positionStyles.leftPosition['&::after'],
+          ...positionStyles.verticalStart['&::after'],
+        },
       }
     case 'left':
       return {
         ...positionStyles.leftPosition,
         ...positionStyles.verticalCenter,
+        '&::after': {
+          ...positionStyles.leftPosition['&::after'],
+          ...positionStyles.verticalCenter['&::after'],
+        },
       }
     case 'leftEnd':
       return {
         ...positionStyles.leftPosition,
         ...positionStyles.verticalEnd,
+        '&::after': {
+          ...positionStyles.leftPosition['&::after'],
+          ...positionStyles.verticalEnd['&::after'],
+        },
       }
   }
 }
