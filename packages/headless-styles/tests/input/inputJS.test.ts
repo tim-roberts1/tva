ed8e51f579799dccb266387f6205ea6133c20534
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-10-20 12:09:50 -0400

    test(headless-styles): update inputJS tests with adjusted sizes

--- packages/headless-styles/tests/input/inputJS.test.ts
+++ packages/headless-styles/tests/input/inputJS.test.ts
@@ -3,20 +3,20 @@ import type { InputOptions } from '../../src/types'
 
 describe('Input JS', () => {
   test('should allow no props to be passed in', () => {
-    expect(getJSInputProps().input.cssProps).toContain('2.9375rem')
-    expect(getJSInputProps().input.styles.height).toEqual('2.9375rem')
+    expect(getJSInputProps().input.cssProps).toContain('3rem')
+    expect(getJSInputProps().input.styles.height).toEqual('3rem')
   })
 
   test('should accept a medium size type', () => {
     const options = {
       size: 'm',
     } as InputOptions
-    expect(getJSInputProps(options).input.cssProps).toContain('height: 2.5rem')
-    expect(getJSInputProps(options).input.styles.height).toEqual('2.5rem')
+    expect(getJSInputProps(options).input.cssProps).toContain('height: 2rem')
+    expect(getJSInputProps(options).input.styles.height).toEqual('2rem')
   })
 
   test('should accept a large size type', () => {
-    expect(getJSInputProps().input.cssProps).toContain('height: 2.9375rem')
-    expect(getJSInputProps().input.styles.height).toEqual('2.9375rem')
+    expect(getJSInputProps().input.cssProps).toContain('height: 3rem')
+    expect(getJSInputProps().input.styles.height).toEqual('3rem')
   })
 })
