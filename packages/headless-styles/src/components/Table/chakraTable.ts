0d2d819efd6eb92a43a4fdbdbb65248d149f2dcc
Casey Baggz <caseybaggz@gmail.com>
2022-10-20 13:03:36 -0500

    feat(hs): update ChakraTable styles

--- packages/headless-styles/src/components/Table/chakraTable.ts
+++ packages/headless-styles/src/components/Table/chakraTable.ts
@@ -1,17 +1,40 @@
 import styles from './generated/tableCSS.module'
 
+const chakraTableCellOverride = {
+  borderColor: 'var(--ps-border)',
+  fontFamily: 'inherit',
+  textTransform: 'initial',
+  letterSpacing: 'inherit',
+  textAlign: 'initial',
+}
+
 export const ChakraTable = {
   baseStyle: {
     table: styles.table,
-    th: styles.headCell,
-    td: styles.bodyCell,
-    caption: styles.caption,
+    th: {
+      ...styles.headCell,
+      ...chakraTableCellOverride,
+    },
+    td: {
+      ...styles.headCell,
+      ...styles.bodyCell,
+      ...chakraTableCellOverride,
+    },
+    tr: styles.tableRow,
+    caption: {
+      ...styles.caption,
+      fontFamily: 'inherit',
+      fontWeight: 500,
+      letterSpacing: 'inherit',
+      textAlign: 'inherit',
+      textTransform: 'initial',
+    },
   },
   variants: {},
   sizes: {},
   defaultProps: {
     variant: 'simple',
-    size: null,
+    size: 'md',
     colorScheme: null,
   },
 }
