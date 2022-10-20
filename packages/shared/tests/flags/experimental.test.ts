8f86a641f732e79c4c7d4bf0503a45e4d4bea70a
Casey Baggz <caseybaggz@gmail.com>
2022-10-20 11:43:26 -0500

    test(shared): fix TS lint error

--- packages/shared/tests/flags/experimental.test.ts
+++ packages/shared/tests/flags/experimental.test.ts
@@ -1,5 +1,8 @@
-// import * as flags from '../../src/featureFlags'
+import * as flags from '../../src/featureFlags'
 
 describe('experimental feature flags', () => {
-  test.todo('should be true for experimental features')
+  test('should be true for experimental features', () => {
+    // we have to have at least one passing test for TS
+    expect(flags.table).toEqual(true)
+  })
 })
