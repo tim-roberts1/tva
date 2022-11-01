0da0d73f5b2e0dd2da7c6c78094153d74d8bc6ab
Casey Baggz <caseybaggz@gmail.com>
2022-11-01 11:26:05 -0500

    chore(hs): fix TS linting error in paginationJS.ts

--- packages/headless-styles/src/components/Pagination/paginationJS.ts
+++ packages/headless-styles/src/components/Pagination/paginationJS.ts
@@ -5,7 +5,7 @@ import { createPaginationProps, getDefaultPaginationOptions } from './shared'
 import styles from './generated/paginationCSS.module'
 import type { PaginationOptions } from './types'
 
-export function getJSPaginationProps(options: PaginationOptions) {
+export function getJSPaginationProps(options?: PaginationOptions) {
   const { cols } = getDefaultPaginationOptions(options)
   const { paginationContainer, paginationNewer, paginationOlder } = styles
   const props = createPaginationProps(cols)
