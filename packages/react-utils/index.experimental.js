9fc75ba4a4ce137949f918b325a67b8de3620779
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-11-11 12:37:51 -0500

    fix(react-utils): fix hook export reference

--- packages/react-utils/index.experimental.js
+++ packages/react-utils/index.experimental.js
@@ -1,7 +1,7 @@
 export { useAutoFormatDate } from './src/hooks/useAutoFormatDate'
 export { useEscToClose } from './src/hooks/useEscToClose'
 export { useFocusTrap } from './src/hooks/useFocusTrap'
-export { useMenuInteraction } from './src/hooks/useMenuInteraction'
+export { useMenuInteraction } from './src/hooks/menu/useMenuInteraction'
 export { usePreloadedImg } from './src/hooks/usePreloadedImg'
 export { useRovingTabIndex } from './src/hooks/useRovingTabIndex'
 
