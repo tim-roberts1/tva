9a9087660acf0e2e7c2fbb871a92779377f14167
Casey Baggz <caseybaggz@gmail.com>
2022-11-15 13:26:57 -0600

    test(hs): add gridCSS.test.ts

---
+++ packages/headless-styles/tests/grid/gridCSS.test.ts
@@ -0,0 +1,76 @@
+import { getGridProps, getGridItemProps } from '../../src/index'
+
+describe('Grid CSS', () => {
+  const gridProps = {
+    className: 'ps-grid gridContainer',
+    style: {
+      gridTemplateRows: 'repeat(1, 1fr)',
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      gap: '1rem',
+    },
+  }
+  const gridItemProps = {
+    className: 'ps-grid-item',
+    style: {
+      gridColumn: 'span 1 / span 1',
+    },
+  }
+
+  describe('getGridProps', () => {
+    test('should allow no props to be passed in', () => {
+      expect(getGridProps()).toEqual(gridProps)
+    })
+
+    test('should update the gap when option given', () => {
+      expect(getGridProps({ gap: 6 })?.style.gap).toEqual('0.375rem')
+      expect(getGridProps({ gap: 8 })?.style.gap).toEqual('0.5rem')
+      expect(getGridProps({ gap: 12 })?.style.gap).toEqual('0.75rem')
+      expect(getGridProps({ gap: 16 })?.style.gap).toEqual('1rem')
+      expect(getGridProps({ gap: 32 })?.style.gap).toEqual('2rem')
+    })
+
+    test('should update the cols when option given', () => {
+      expect(getGridProps({ cols: 12 })?.style.gridTemplateColumns).toEqual(
+        'repeat(12, 1fr)'
+      )
+      expect(getGridProps({ cols: 8 })?.style.gridTemplateColumns).toEqual(
+        'repeat(8, 1fr)'
+      )
+    })
+
+    test('should accept a tech type', () => {
+      expect(getGridProps({ tech: 'svelte' })).toEqual({
+        class: gridProps.className,
+        style: gridProps.style,
+      })
+    })
+  })
+
+  describe('getGridItemProps', () => {
+    test('should allow no props to be passed in', () => {
+      expect(getGridItemProps()).toEqual(gridItemProps)
+    })
+
+    test('should update the colSpan when option given', () => {
+      expect(getGridItemProps({ colSpan: 12 })?.style.gridColumn).toEqual(
+        'span 12 / span 12'
+      )
+      expect(getGridItemProps({ colSpan: 8 })?.style.gridColumn).toEqual(
+        'span 8 / span 8'
+      )
+    })
+
+    test('should update the rowSpan when option given', () => {
+      expect(getGridItemProps({ rowSpan: 12 })?.style.gridArea).toEqual(
+        'span 12 / span 1 / span 12 / span 1'
+      )
+    })
+
+    test('should accept a tech type', () => {
+      expect(getGridItemProps({ tech: 'svelte' })).toEqual({
+        class: gridItemProps.className,
+        style: gridItemProps.style,
+      })
+    })
+  })
+})
