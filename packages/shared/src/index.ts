9b730290320785f51f1beed24b72c92cad7074a0
Casey Baggz <caseybaggz@gmail.com>
2022-11-22 08:45:44 -0600

    chore: remove unused imports in shared entry

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,17 +1,3 @@
-export {
-  grid,
-  menu,
-  menuHook,
-  modal,
-  pagination,
-  popover,
-  preloadImgHook,
-  select,
-  submenuHook,
-  tabindexHook,
-  table,
-  tabs,
-  tabsHook,
-} from './featureFlags'
+export { popover } from './featureFlags'
 
 export { warning } from './utils/helpers'
