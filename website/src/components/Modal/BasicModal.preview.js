b1534248cebf06b1586f9c63af9dc3679fbd267b
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-12 16:20:07 -0500

    docs: fix indentation in modal full code preview

--- website/src/components/Modal/BasicModal.preview.js
+++ website/src/components/Modal/BasicModal.preview.js
@@ -26,60 +26,60 @@ export function BasicModalPreview() {
 export function BasicModalFullPreview() {
   return (
     <CodeBlock>{`import React, { forwardRef, memo, useEffect, useRef } from 'react'
-    import { useEscToClose, useFocusTrap } from '@pluralsight/react-utils'
-    import {
-      getIconButtonProps,
-      getIconProps,
-      getModalProps,
-    } from '@pluralsight/headless-styles'
-    import { CloseIcon } from '@pluralsight/icons'
+import { useEscToClose, useFocusTrap } from '@pluralsight/react-utils'
+import {
+  getIconButtonProps,
+  getIconProps,
+  getModalProps,
+} from '@pluralsight/headless-styles'
+import { CloseIcon } from '@pluralsight/icons'
 
-    function ModalEl(props, triggerRef) {
-      const { onClose, ...modalProps } = props
-      const modal = getModalProps(modalProps)
-      console.log(modal)
-      const { button, iconOptions } = getIconButtonProps(modal.cancelBtnOptions)
-      const wrapperRef = useRef(null)
-      const { ref, onKeyDown, setupFocusTrap } = useFocusTrap(triggerRef)
+function ModalEl(props, triggerRef) {
+  const { onClose, ...modalProps } = props
+  const modal = getModalProps(modalProps)
+  console.log(modal)
+  const { button, iconOptions } = getIconButtonProps(modal.cancelBtnOptions)
+  const wrapperRef = useRef(null)
+  const { ref, onKeyDown, setupFocusTrap } = useFocusTrap(triggerRef)
 
-      function handleBackdropClick(event) {
-        event.stopPropagation()
-        if (wrapperRef.current === event.target) {
-          onClose()
-        }
-      }
-
-      useEscToClose(onClose)
-
-      useEffect(() => {
-        setupFocusTrap()
-      }, [setupFocusTrap])
-
-      return (
-        <div {...modal.backdrop}>
-          <div {...modal.focusGuard} />
-
-          <div {...modal.wrapper} ref={wrapperRef} onClick={handleBackdropClick}>
-            <section {...modal.section} ref={ref} onKeyDown={onKeyDown}>
-              <header>
-                <h6 {...modal.modalHeading}>{props.heading}</h6>
-              </header>
-              <div {...modal.modalBody}>{props.body}</div>
-              <footer {...modal.buttonWrapper}>
-                <button {...button}>
-                  <CloseIcon {...getIconProps(iconOptions)} onClick={onClose} />
-                </button>
-              </footer>
-            </section>
-          </div>
-
-          <div {...modal.focusGuard} />
-        </div>
-      )
+  function handleBackdropClick(event) {
+    event.stopPropagation()
+    if (wrapperRef.current === event.target) {
+      onClose()
     }
+  }
 
-    const Modal = memo(forwardRef(ModalEl))
+  useEscToClose(onClose)
 
-    export default Modal`}</CodeBlock>
+  useEffect(() => {
+    setupFocusTrap()
+  }, [setupFocusTrap])
+
+  return (
+    <div {...modal.backdrop}>
+      <div {...modal.focusGuard} />
+
+      <div {...modal.wrapper} ref={wrapperRef} onClick={handleBackdropClick}>
+        <section {...modal.section} ref={ref} onKeyDown={onKeyDown}>
+          <header>
+            <h6 {...modal.modalHeading}>{props.heading}</h6>
+          </header>
+          <div {...modal.modalBody}>{props.body}</div>
+          <footer {...modal.buttonWrapper}>
+            <button {...button}>
+              <CloseIcon {...getIconProps(iconOptions)} onClick={onClose} />
+            </button>
+          </footer>
+        </section>
+      </div>
+
+      <div {...modal.focusGuard} />
+    </div>
+  )
+}
+
+const Modal = memo(forwardRef(ModalEl))
+
+export default Modal`}</CodeBlock>
   )
 }
