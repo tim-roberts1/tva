4bcd4c452497e9af15d8d659c6883095de2b63f5
Casey Baggz <caseybaggz@gmail.com>
2022-12-12 10:13:11 -0600

    docs: update SelectSizes preview

--- website/src/components/Select/SelectSizes.preview.js
+++ website/src/components/Select/SelectSizes.preview.js
@@ -5,24 +5,33 @@ import selectPreview from './Select.preview'
 export function SelectSizesPreview() {
   return (
     <CodeBlock>{`const { fieldOptions } = getFormControlProps()
+const { value as mLabel, ...labelProps as mLabelProps } = getFormLabelProps({
+  htmlFor: 'medium-select',
+  value: 'Medium select',
+})
 const mSelectProps = getSelectProps({
   ...fieldOptions,
   id: 'medium-select',
   name: 'medium_select',
-  placeholder: 'Medium select',
   size: 'm',
   value: ''
+})
+const { value as lLabel, ...labelProps as lLabelProps } = getFormLabelProps({
+  htmlFor: 'large-select',
+  value: 'Large select',
 })
 const selectProps = getSelectProps({
   ...fieldOptions,
   id: 'large-select',
   name: 'large_select',
-  placeholder: 'Large select',
   size: 'l',
   value: ''
 })
 
+<label {...mLabelProps}>{mLabel}</label>
 <select {...mSelectProps.select} onChange={handleCheck} />
+
+<label {...lLabelProps}>{lLabel}</label>
 <select {...selectProps.select} onChange={handleCheck} />`}</CodeBlock>
   )
 }
