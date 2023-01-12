84a8959532a4074fd5d52a917eb160a851dd7d5c
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2023-01-11 21:51:56 -0600

    feat(react-utils): add ability to pass an initial theme value and add additional validation

--- packages/react-utils/src/hooks/useTheme.ts
+++ packages/react-utils/src/hooks/useTheme.ts
@@ -1,22 +1,42 @@
 import { useEffect, useState, useCallback } from 'react'
+import { isThemeValid } from '../hooks/menu/utils'
 
-const initialTheme = localStorage.getItem('ps-theme') || 'light'
+const cachedTheme = localStorage.getItem('ps-theme')
 
-export function useTheme() {
+export function useTheme(preferredTheme?: 'light' | 'dark') {
+  const themes = {
+    light: 'light',
+    dark: 'dark',
+  }
+
+  const getInitialTheme = () => {
+    if (
+      preferredTheme &&
+      isThemeValid(preferredTheme) &&
+      cachedTheme === null
+    ) {
+      return themes[preferredTheme]
+    }
+    return cachedTheme || themes.light
+  }
+
+  const initialTheme = getInitialTheme()
   const [theme, setTheme] = useState(initialTheme)
   const { documentElement } = document
 
-  if (documentElement.getAttribute('ps-theme') === null) {
+  if (documentElement.getAttribute('ps-theme') === null && theme !== null) {
     documentElement.setAttribute('ps-theme', theme)
   }
 
   useEffect(() => {
-    document.documentElement.setAttribute('ps-theme', theme)
-    localStorage.setItem('ps-theme', theme)
+    if (isThemeValid(theme)) {
+      document.documentElement.setAttribute('ps-theme', theme)
+      localStorage.setItem('ps-theme', theme)
+    }
   }, [theme])
 
   const toggleTheme = useCallback(() => {
-    setTheme((prev) => {
+    setTheme((prev: string) => {
       if (prev === 'dark') {
         return 'light'
       }
