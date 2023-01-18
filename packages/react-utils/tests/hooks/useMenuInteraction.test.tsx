0d6de3fb451e35d8602ec9954490ac16f95b4e56
Casey Baggz <caseybaggz@gmail.com>
2023-01-18 13:25:57 -0600

    test(react-utils): fix act tests in useMenuInteraction.test.tsx

--- packages/react-utils/tests/hooks/useMenuInteraction.test.tsx
+++ packages/react-utils/tests/hooks/useMenuInteraction.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, userEvent } from 'test-utils'
+import { render, screen, userEvent, waitFor } from 'test-utils'
 import { useMenuInteraction } from '../../src'
 
 type MenuProps = {
@@ -21,25 +21,24 @@ describe('useMenuInteraction', () => {
 
     return (
       <div>
-        <button data-testid="trigger" {...menuNavProps?.trigger}>
-          {props.label}
-        </button>
-        <menu aria-label={props.label} {...menuNavProps?.menu}>
-          <MenuItem label="Menu item 1" />
-          <MenuItem label="Menu item 2" />
-          <MenuItem label="Menu item 3" />
-          <MenuItem label="Menu item 4" />
-          <MenuItem label="Menu item 5" />
-        </menu>
+        <button {...menuNavProps?.trigger}>{props.label}</button>
+        {menuNavProps.expanded && (
+          <menu aria-label={props.label} {...menuNavProps?.menu}>
+            <MenuItem label="Menu item 1" />
+            <MenuItem label="Menu item 2" />
+            <MenuItem label="Menu item 3" />
+            <MenuItem label="Menu item 4" />
+            <MenuItem label="Menu item 5" />
+          </menu>
+        )}
       </div>
     )
   }
 
-  function MenuTest() {
+  function MenuTest(props: MenuProps) {
     return (
       <>
-        <Menu label="Menu" />
-
+        <Menu label={props.label} />
         <button>button after menu</button>
       </>
     )
@@ -49,174 +48,226 @@ describe('useMenuInteraction', () => {
   const menuExpanded = 'data-expanded'
   const arrowUp = '{ArrowUp}'
   const arrowDown = '{ArrowDown}'
-  const esc = '{Escape}'
 
   test('should have the correct initial aria attributes present', () => {
-    const menu = screen.getByRole('menu')
-    render(<MenuTest />)
+    render(<MenuTest label="one" />)
 
-    const trigger = screen.getByTestId('trigger')
-
-    expect(menu).toHaveAttribute(menuExpanded, 'false')
-    expect(menu).toHaveAttribute('role', 'menu')
+    const trigger = screen.getByText(/one/i)
     expect(trigger).toHaveAttribute(triggerExpanded, 'false')
     expect(trigger).toHaveAttribute('aria-haspopup', 'true')
   })
 
   test('should display menu when trigger clicked', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="two" />)
 
-    const menu = screen.getByRole('menu')
-    const trigger = screen.getByTestId('trigger')
+    const trigger = screen.getByText(/two/i)
 
-    await user.click(trigger)
+    user.click(trigger)
 
-    expect(menu).toHaveAttribute(menuExpanded, 'true')
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
+
     expect(trigger).toHaveAttribute(triggerExpanded, 'true')
-    expect(trigger).toHaveFocus()
   })
 
   test('should display menu and focus first item when down arrow pressed on trigger', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="three" />)
 
-    const menu = screen.getByRole('menu')
-    const trigger = screen.getByTestId('trigger')
-    const firstMenuItem = screen.getAllByRole('menuitem')[0]
-
+    const trigger = screen.getByText(/three/i)
     trigger.focus()
-    await user.keyboard(arrowDown)
 
-    expect(menu).toHaveAttribute(menuExpanded, 'true')
+    user.keyboard(arrowDown)
+
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
+
     expect(trigger).toHaveAttribute(triggerExpanded, 'true')
-    expect(firstMenuItem).toHaveFocus()
+    expect(screen.getByText(/menu item 1/i)).toHaveFocus()
   })
 
   test('should close menu and focus on trigger item when escape pressed within menu', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="four" />)
 
-    const menu = screen.getByRole('menu')
-    const trigger = screen.getByTestId('trigger')
-
+    const trigger = screen.getByText(/four/i)
     trigger.focus()
-    await user.keyboard(arrowDown)
-    await user.keyboard(esc)
 
-    expect(menu).toHaveAttribute(menuExpanded, 'false')
+    user.keyboard(arrowDown)
+
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
+
+    await waitFor(() => user.keyboard('{Escape}'))
+    await waitFor(() => expect(menu).not.toBeInTheDocument())
+
     expect(trigger).toHaveAttribute(triggerExpanded, 'false')
     expect(trigger).toHaveFocus()
   })
 
   test('should close menu and move focus to next focusable element outside of the menu when tab is pressed', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="five" />)
 
-    const menu = screen.getByRole('menu')
-    const trigger = screen.getByTestId('trigger')
-    const buttonAfterMenu = screen.getByRole('button', {
-      name: 'button after menu',
-    })
-
+    const trigger = screen.getByText(/five/i)
     trigger.focus()
-    await user.keyboard(arrowDown)
-    await user.keyboard('{Tab}')
 
-    expect(menu).toHaveAttribute(menuExpanded, 'false')
-    expect(trigger).toHaveAttribute(triggerExpanded, 'false')
-    expect(buttonAfterMenu).toHaveFocus()
-  })
-
-  test('should close when focus moves outside the menu and trigger', async () => {
-    const user = userEvent.setup()
-    render(<MenuTest />)
-
-    const menu = screen.getByRole('menu')
-    const trigger = screen.getByTestId('trigger')
-    const nextFocusableElement = screen.getByRole('button', {
-      name: 'button after menu',
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
     })
-    const items = screen.getAllByRole('menuitem')
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    await user.click(trigger)
-    await items[0].focus()
-    await nextFocusableElement.focus()
+    await waitFor(() => user.keyboard('{Tab}'))
+    await waitFor(() => expect(menu).not.toBeInTheDocument())
 
-    expect(menu).toHaveAttribute(menuExpanded, 'false')
     expect(trigger).toHaveAttribute(triggerExpanded, 'false')
+
+    const afterButton = await screen.findByText(
+      /button after menu/i,
+      undefined,
+      {
+        mutationObserverOptions: {
+          attributes: true,
+        },
+      }
+    )
+    await waitFor(() => expect(afterButton).toHaveFocus())
   })
 
-  test('should move focus to next item when down arrow pressed', async () => {
+  test('should move focus to next item when down arrow pressed when open', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="seven" />)
 
-    const items = screen.getAllByRole('menuitem')
+    const trigger = screen.getByText(/seven/i)
+    trigger.focus()
 
-    items[0].focus()
-    await user.keyboard(arrowDown)
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    expect(items[1]).toHaveFocus()
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => expect(screen.getByText(/menu item 2/i)).toHaveFocus())
   })
 
   test('should move focus to first item when down arrow pressed on last item', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="eight" />)
 
-    const items = screen.getAllByRole('menuitem')
+    const trigger = screen.getByText(/eight/i)
+    trigger.focus()
 
-    items[items.length - 1].focus()
-    await user.keyboard(arrowDown)
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    expect(items[0]).toHaveFocus()
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => expect(screen.getByText(/menu item 1/i)).toHaveFocus())
   })
 
   test('should move focus to previous item when up arrow pressed', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="nein!" />)
 
-    const items = screen.getAllByRole('menuitem')
+    const trigger = screen.getByText(/nein!/i)
+    trigger.focus()
 
-    items[1].focus()
-    await user.keyboard(arrowUp)
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    expect(items[0]).toHaveFocus()
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => user.keyboard(arrowDown))
+    await waitFor(() => user.keyboard(arrowUp))
+    await waitFor(() => expect(screen.getByText(/menu item 2/i)).toHaveFocus())
   })
 
   test('should move focus to last item when up arrow pressed on first item', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="ten" />)
 
-    const items = screen.getAllByRole('menuitem')
+    const trigger = screen.getByText(/ten/i)
+    trigger.focus()
 
-    items[0].focus()
-    await user.keyboard(arrowUp)
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    expect(items[items.length - 1]).toHaveFocus()
+    await waitFor(() => user.keyboard(arrowUp))
+    await waitFor(() => expect(screen.getByText(/menu item 5/i)).toHaveFocus())
   })
 
   test('should move focus to first item when home key pressed', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="el" />)
 
-    const items = screen.getAllByRole('menuitem')
+    const trigger = screen.getByText(/el/i)
+    trigger.focus()
 
-    items[2].focus()
-    await user.keyboard('{Home}')
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    expect(items[0]).toHaveFocus()
+    await waitFor(() => user.keyboard('{Home}'))
+    await waitFor(() => expect(screen.getByText(/menu item 1/i)).toHaveFocus())
   })
 
   test('should move focus to last item when end key pressed', async () => {
     const user = userEvent.setup()
-    render(<MenuTest />)
+    render(<MenuTest label="el" />)
 
-    const items = screen.getAllByRole('menuitem')
+    const trigger = screen.getByText(/el/i)
+    trigger.focus()
 
-    items[2].focus()
-    await user.keyboard('{End}')
+    await waitFor(() => user.keyboard(arrowDown))
+    const menu = await screen.findByRole('menu', undefined, {
+      mutationObserverOptions: {
+        attributes: true,
+      },
+    })
+    await waitFor(() => expect(menu).toHaveAttribute(menuExpanded, 'true'))
 
-    expect(items[items.length - 1]).toHaveFocus()
+    await waitFor(() => user.keyboard('{End}'))
+    await waitFor(() => expect(screen.getByText(/menu item 5/i)).toHaveFocus())
   })
 
   test.todo(
