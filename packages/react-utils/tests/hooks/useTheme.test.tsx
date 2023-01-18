611703bf41bbec253a59cc1cb1a090c8f55a0f2f
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2023-01-17 18:14:36 -0600

    test(react-utils): remove unnecessary useTheme test cases

--- packages/react-utils/tests/hooks/useTheme.test.tsx
+++ packages/react-utils/tests/hooks/useTheme.test.tsx
@@ -7,6 +7,8 @@ describe('useTheme', () => {
     localStorage.clear()
   })
 
+  const KEY = 'data-theme'
+
   function TestComponent() {
     const { toggleTheme } = useTheme()
     return (
@@ -25,62 +27,32 @@ describe('useTheme', () => {
     return { user, button }
   }
 
-  test('should set the theme to light', () => {
-    render(<TestComponent />)
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'light')
-  })
-
   test('should set the theme to dark', async () => {
     const { user, button } = setup()
     await user.click(button)
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'dark')
+    expect(document.documentElement).toHaveAttribute(KEY, 'dark')
   })
 
   test('should set the theme back to light when it is already set to dark', async () => {
     const { user, button } = setup()
     await user.click(button)
     await user.click(button)
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'light')
+    expect(document.documentElement).toHaveAttribute(KEY, 'light')
   })
 
   test('should persist the theme in local storage when it is changed to dark', async () => {
     const { user, button } = setup()
     await user.click(button)
-    expect(localStorage.getItem('ps-theme')).toBe('dark')
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'dark')
+    expect(localStorage.getItem(KEY)).toBe('dark')
+    expect(document.documentElement).toHaveAttribute(KEY, 'dark')
   })
 
   test('should persist the theme in local storage when it is changed to light', async () => {
     const { user, button } = setup()
     await user.click(button)
     await user.click(button)
-    expect(localStorage.getItem('ps-theme')).toBe('light')
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'light')
-  })
-
-  test('should set the theme to light when the document element does not have a ps-theme attribute', () => {
-    document.documentElement.removeAttribute('ps-theme')
-    render(<TestComponent />)
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'light')
-  })
-
-  test('should set the theme to light if the document element has an invalid ps-theme attribute', () => {
-    document.documentElement.setAttribute('ps-theme', 'invalid')
-    render(<TestComponent />)
-    expect(document.documentElement).toHaveAttribute('ps-theme', 'light')
-  })
-
-  test('should be dark theme if the useTheme hook is provided an inital value of dark', () => {
-    const TestComponent = () => {
-      const { theme } = useTheme('dark')
-      return (
-        <div>
-          <div data-testid="theme">{theme}</div>
-        </div>
-      )
-    }
-    render(<TestComponent />)
-    expect(screen.getByTestId('theme')).toHaveTextContent('dark')
+    expect(localStorage.getItem(KEY)).toBe('light')
+    expect(document.documentElement).toHaveAttribute(KEY, 'light')
   })
 
   test('should be light theme if the useTheme hook is provided an inital value of light', () => {
@@ -96,10 +68,9 @@ describe('useTheme', () => {
     expect(screen.getByTestId('theme')).toHaveTextContent('light')
   })
 
-  test('should be light theme if the useTheme hook is provided an inital value that is invalid', () => {
+  test('should be dark theme if the useTheme hook is provided an inital value of dark', () => {
     const TestComponent = () => {
-      // @ts-expect-error
-      const { theme } = useTheme('invalid')
+      const { theme } = useTheme('dark')
       return (
         <div>
           <div data-testid="theme">{theme}</div>
@@ -107,6 +78,6 @@ describe('useTheme', () => {
       )
     }
     render(<TestComponent />)
-    expect(screen.getByTestId('theme')).toHaveTextContent('light')
+    expect(screen.getByTestId('theme')).toHaveTextContent('dark')
   })
 })
