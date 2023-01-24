aafb361c5bbdf9de4de8ad729d3bfc4540affe43
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-01-24 11:43:43 -0500

    docs(headless-styles): clean up select code preview

--- website/src/components/Select/BasicSelect.preview.js
+++ website/src/components/Select/BasicSelect.preview.js
@@ -12,9 +12,9 @@ const selectProps = getSelectProps({
   value: ''
 })
 
-<div {...selectProps.fieldWrapper>
-  <div {...selectProps.selectWrapper>
-    <select {...selectProps.select} onChange={handleCheck}>
+<div {...selectProps.fieldWrapper}>
+  <div {...selectProps.selectWrapper}>
+    <select {...selectProps.select}>
       <option {...getSelectOptionProps({value: 'option1'})}>Option 1</option>
       <option {...getSelectOptionProps({value: 'option2'})}>Option 2</option>
     </select>
