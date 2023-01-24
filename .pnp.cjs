dba9ac62f71d8d0e17b1c0bb78294a7e3d3a0ff4
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-01-24 11:43:09 -0500

    docs: up HS dependency

--- .pnp.cjs
+++ .pnp.cjs
@@ -1062,7 +1062,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ],\
       [\
         "@pluralsight/headless-styles",\
-        "npm:0.3.1-rc-8fbb74"\
+        "npm:0.3.1-rc-33cc5a"\
       ],\
       [\
         "@pluralsight/icons",\
@@ -20815,10 +20815,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@pluralsight/headless-styles", [\
-        ["npm:0.3.1-rc-8fbb74", {\
-          "packageLocation": "./.yarn/cache/@pluralsight-headless-styles-npm-0.3.1-rc-8fbb74-f26147940f-a37962b393.zip/node_modules/@pluralsight/headless-styles/",\
+        ["npm:0.3.1-rc-33cc5a", {\
+          "packageLocation": "./.yarn/cache/@pluralsight-headless-styles-npm-0.3.1-rc-33cc5a-65354c011e-85c3e7f3c6.zip/node_modules/@pluralsight/headless-styles/",\
           "packageDependencies": [\
-            ["@pluralsight/headless-styles", "npm:0.3.1-rc-8fbb74"],\
+            ["@pluralsight/headless-styles", "npm:0.3.1-rc-33cc5a"],\
             ["tslib", "npm:2.4.1"]\
           ],\
           "linkType": "HARD"\
@@ -42895,7 +42895,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@docusaurus/theme-live-codeblock", "virtual:8693780468136bf004f9b532d653844c342247b6a39276486a4e8cbecc2c295c87d04c42b26f9c469bc9dfcf5a0d1f6012e10b6c148cd44051b30c652ede2384#npm:2.2.0"],\
             ["@mdx-js/react", "virtual:8693780468136bf004f9b532d653844c342247b6a39276486a4e8cbecc2c295c87d04c42b26f9c469bc9dfcf5a0d1f6012e10b6c148cd44051b30c652ede2384#npm:1.6.22"],\
             ["@pluralsight/design-tokens", "npm:0.3.1-rc-5d4a4f"],\
-            ["@pluralsight/headless-styles", "npm:0.3.1-rc-8fbb74"],\
+            ["@pluralsight/headless-styles", "npm:0.3.1-rc-33cc5a"],\
             ["@pluralsight/icons", "virtual:8693780468136bf004f9b532d653844c342247b6a39276486a4e8cbecc2c295c87d04c42b26f9c469bc9dfcf5a0d1f6012e10b6c148cd44051b30c652ede2384#npm:0.1.2-beta-22b9c2"],\
             ["@pluralsight/react-utils", "virtual:8693780468136bf004f9b532d653844c342247b6a39276486a4e8cbecc2c295c87d04c42b26f9c469bc9dfcf5a0d1f6012e10b6c148cd44051b30c652ede2384#npm:0.3.1-rc-5d4a4f"],\
             ["@svgr/webpack", "npm:5.5.0"],\
