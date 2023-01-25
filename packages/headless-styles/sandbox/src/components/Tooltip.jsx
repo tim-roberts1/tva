c4ddf1f02a088869b8ccd6dde0a4e947efa0c806
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-01-25 11:57:15 -0500

    test(headless-styles): update tooltip sandbox to include JS API case

--- packages/headless-styles/sandbox/src/components/Tooltip.jsx
+++ packages/headless-styles/sandbox/src/components/Tooltip.jsx
@@ -35,6 +35,92 @@ function TooltipEl(props) {
   )
 }
 
+function TooltipJS(props) {
+  const [disabled, setDisabled] = useState(false)
+  const [tooltipStyle, setTooltipStyle] = useState({})
+  const [triggerStyle, setTriggerStyle] = useState({})
+
+  const tooltipProps = getJSTooltipProps({
+    id: props.id,
+    position: props.position,
+    disabled: disabled,
+  })
+
+  const disable = useCallback(() => {
+    setDisabled(true)
+  }, [])
+
+  function enable() {
+    setDisabled(false)
+  }
+
+  function show() {
+    if (!disabled) {
+      setTooltipStyle({
+        ...tooltipProps.tooltip.styles,
+        ...tooltipProps.tooltip.styles["&:not([data-disabled='true']):hover"],
+        opacity: 1,
+      })
+    }
+  }
+
+  function hide() {
+    if (!disabled) {
+      setTooltipStyle(tooltipProps.tooltip.styles)
+    }
+  }
+
+  function focus() {
+    show()
+    setTriggerStyle({
+      ...tooltipProps.trigger.styles,
+      ...tooltipProps.trigger.styles['&:focus'],
+    })
+  }
+
+  function blur() {
+    hide()
+    setTriggerStyle(tooltipProps.trigger.styles)
+  }
+
+  useEscToClose(disable)
+
+  useEffect(() => {
+    setTooltipStyle(tooltipProps.tooltip.styles)
+    setTriggerStyle(tooltipProps.trigger.styles)
+  }, [])
+
+  return (
+    <div
+      style={tooltipProps.wrapper.styles}
+      onMouseEnter={enable}
+      onFocus={enable}
+      onMouseOver={show}
+      onMouseLeave={hide}
+      {...tooltipProps.wrapper.a11yProps}
+    >
+      <div
+        style={triggerStyle}
+        onFocus={focus}
+        onBlur={blur}
+        {...tooltipProps.trigger.a11yProps}
+      >
+        {props.children}
+      </div>
+      <div style={tooltipStyle} {...tooltipProps.tooltip.a11yProps}>
+        <div
+          style={tooltipProps.tooltipContent.styles}
+          {...tooltipProps.tooltipContent.a11yProps}
+        >
+          Sample tooltip text goes here.
+          <span style={tooltipProps.tooltipContent.styles['&::after']} />
+        </div>
+        <span style={tooltipProps.tooltip.styles['&::after']} />
+      </div>
+    </div>
+  )
+}
+
 export default function Tooltip({ logJS }) {
   useEffect(() => {
     if (logJS) {
@@ -49,12 +135,25 @@ export default function Tooltip({ logJS }) {
   return (
     <div id="tooltip">
       <h2>Tooltips</h2>
+
+      <h3>CSS</h3>
       <SimpleGrid cols="3">
         {positions.map((position) => (
           <div key={position}>
             <TooltipEl id={`tooltip:${position}`} position={position}>
               {position}
             </TooltipEl>
+          </div>
+        ))}
+      </SimpleGrid>
+
+      <h3>JS</h3>
+      <SimpleGrid cols="3">
+        {positions.map((position) => (
+          <div key={position}>
+            <TooltipJS id={`tooltip:${position}`} position={position}>
+              {position}
+            </TooltipJS>
           </div>
         ))}
       </SimpleGrid>
