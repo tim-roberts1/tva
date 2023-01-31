011ada2dc679e762644669a6f8d1e17488543cb0
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-01-31 16:29:06 -0500

    docs(headless-styles): add section to Select docs regarding placeholder option

--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -14,6 +14,7 @@ import {
   getPopoverProps,
   getRadioProps,
   getSelectProps,
+  getSelectOptionProps,
   getTagProps,
 } from '@pluralsight/headless-styles'
 import {
@@ -50,6 +51,7 @@ const ReactLiveScope = {
   getPopoverProps,
   getRadioProps,
   getSelectProps,
+  getSelectOptionProps,
   getTagProps,
   // icons
   CalendarIcon,
