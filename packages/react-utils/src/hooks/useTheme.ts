c667d40324f42e195b042f44606e433e18bd4ef7
Casey Baggz <caseybaggz@gmail.com>
2023-02-06 13:15:54 -0600

    fix(react-utils): update useTheme to check for client

--- packages/react-utils/src/hooks/useTheme.ts
+++ packages/react-utils/src/hooks/useTheme.ts
@@ -3,9 +3,16 @@ import { getCachedTheme, setCachedTheme } from '../helpers/themeHelpers'
 import type { Themes } from '../types'
 
 export function useTheme(initialTheme?: Themes) {
-  const [theme, setTheme] = useState<Themes>(
-    () => initialTheme ?? getCachedTheme()
-  )
+  const [isBrowser, setIsBrowser] = useState<boolean>(false)
+  const [theme, setTheme] = useState<Themes>(() => {
+    return initialTheme ?? (isBrowser ? getCachedTheme() : 'dark')
+  })
+
+  // Prevent SSR hydration issues with more reliable check than using
+  // traditional window condition.
+  useEffect(() => {
+    setIsBrowser(true)
+  }, [])
 
   useEffect(() => {
     setCachedTheme(theme)
