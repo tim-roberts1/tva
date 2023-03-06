b24e65739e7a28fe7353c98bfe8a8ea39f213598
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-03-06 12:07:34 -0500

    test(headless-styles): update pagination sandbox for new configuration and add js example

--- packages/headless-styles/sandbox/src/components/Pagination.jsx
+++ packages/headless-styles/sandbox/src/components/Pagination.jsx
@@ -1,59 +1,230 @@
-import { getPaginationProps } from '../../../src'
+import {
+  getButtonProps,
+  getIconButtonProps,
+  getIconProps,
+  getPaginationProps,
+  getSelectProps,
+  getSelectOptionProps,
+  getJSButtonProps,
+  getJSIconButtonProps,
+  getJSIconProps,
+  getJSPaginationProps,
+  getJSSelectProps,
+  getJSSelectOptionProps,
+} from '../../../src'
+import {
+  CaretRightIcon,
+  CaretLeftIcon,
+  ChevronDownIcon,
+} from '@pluralsight/icons'
 
 const styles = getPaginationProps()
-const allStyles = getPaginationProps({
-  cols: 3,
-})
 
 function PaginationInfo() {
   return (
-    <>
-      <small {...styles.text}>
-        <strong>1-25</strong> of 1,234
-      </small>
-    </>
+    <small {...styles.text}>
+      <strong>1-25</strong> of 1,234
+    </small>
   )
 }
 
-function SelectPlaceholder() {
+function Option(props) {
+  const optionProps = getSelectOptionProps()
   return (
-    <div
-      style={{
-        backgroundColor: 'var(--ps-surface-weak)',
-        border: '1px solid var(--ps-border)',
-        borderRadius: '6px',
-        height: '2rem',
-        width: '18.75rem',
-      }}
-    />
+    <option {...optionProps} value={props.value}>
+      {props.children}
+    </option>
+  )
+}
+
+function Select(props) {
+  const selectStyles = getSelectProps({
+    ...styles.selectOptions,
+    id: 'row-count',
+    name: 'row_count',
+    value: props.value,
+  })
+
+  return (
+    <div {...selectStyles.fieldWrapper}>
+      <div {...selectStyles.selectWrapper}>
+        <select {...selectStyles.select} onChange={props.onChange}>
+          <Option value="5">5 Rows</Option>
+          <Option value="10">10 Rows</Option>
+          <Option value="25">25 Rows</Option>
+        </select>
+        <span {...selectStyles.iconWrapper}>
+          <ChevronDownIcon {...getIconProps(selectStyles.iconOptions)} />
+        </span>
+      </div>
+    </div>
   )
 }
 
 function PaginationButtons() {
   return (
-    <>
-      <button {...styles.newer} data-disabled>
+    <div {...styles.buttonGroup}>
+      <button
+        {...getButtonProps({
+          ...styles.buttonOptions,
+          disabled: true,
+        }).button}
+      >
         Newer
       </button>
-      <button {...styles.older}>Older</button>
-    </>
+      <button {...getButtonProps(styles.buttonOptions).button}>Older</button>
+    </div>
+  )
+}
+
+function ArrowButtons() {
+  const prevButtonProps = getIconButtonProps({
+    ...styles.iconButtonOptions,
+    ariaLabel: 'Newer',
+    disabled: true,
+  })
+  const nextButtonProps = getIconButtonProps({
+    ...styles.iconButtonOptions,
+    ariaLabel: 'Older',
+  })
+
+  return (
+    <div {...styles.buttonGroup}>
+      <button {...prevButtonProps.button}>
+        <CaretLeftIcon {...getIconProps(prevButtonProps.iconOptions)} />
+      </button>
+      <button {...nextButtonProps.button}>
+        <CaretRightIcon {...getIconProps(nextButtonProps.iconOptions)} />
+      </button>
+    </div>
   )
 }
 
 function AllPagination() {
   return (
-    <div className="App-container column">
-      <div {...allStyles.container}>
-        <div>
-          <PaginationInfo />
-        </div>
-        <div>
-          <SelectPlaceholder />
-        </div>
-        <div>
-          <PaginationButtons />
-        </div>
+    <div {...styles.container}>
+      <PaginationInfo />
+      <Select />
+      <ArrowButtons />
+    </div>
+  )
+}
+
+// JS -----------
+
+const stylesJS = getJSPaginationProps()
+
+function JSPaginationInfo() {
+  return (
+    <small style={stylesJS.text.styles}>
+      <strong style={stylesJS.text.styles['& > strong']}>1-25</strong> of 1,234
+    </small>
+  )
+}
+
+function JSOption(props) {
+  const optionProps = getJSSelectOptionProps()
+  return (
+    <option style={optionProps.styles} value={props.value}>
+      {props.children}
+    </option>
+  )
+}
+
+function JSSelect(props) {
+  const selectStyles = getJSSelectProps({
+    ...stylesJS.selectOptions,
+    id: 'row-count-js',
+    name: 'row_count_js',
+    value: props.value,
+  })
+  const iconProps = getJSIconProps(selectStyles.iconOptions)
+
+  return (
+    <div style={selectStyles.fieldWrapper.styles}>
+      <div style={selectStyles.selectWrapper.styles}>
+        <select style={selectStyles.select.styles} onChange={props.onChange}>
+          <JSOption value="5">5 Rows</JSOption>
+          <JSOption value="10">10 Rows</JSOption>
+          <JSOption value="25">25 Rows</JSOption>
+        </select>
+        <span style={selectStyles.iconWrapper.styles}>
+          <ChevronDownIcon style={iconProps.styles} {...iconProps.a11yProps} />
+        </span>
       </div>
+    </div>
+  )
+}
+
+function JSPaginationButtons() {
+  const newerButtonProps = getJSButtonProps({
+    ...stylesJS.buttonOptions,
+    disabled: true,
+  })
+  const olderButtonProps = getJSButtonProps({
+    ...stylesJS.buttonOptions,
+  })
+  console.log(newerButtonProps)
+
+  return (
+    <div style={stylesJS.buttonGroup.styles}>
+      <button
+        disabled
+        style={{
+          ...newerButtonProps.button.styles,
+          ...newerButtonProps.button.styles['&:disabled'],
+        }}
+      >
+        Newer
+      </button>
+      <button style={olderButtonProps.button.styles}>Older</button>
+    </div>
+  )
+}
+
+function JSArrowButtons() {
+  const prevButtonProps = getJSIconButtonProps({
+    ...stylesJS.iconButtonOptions,
+    ariaLabel: 'Newer',
+    disabled: true,
+  })
+  const nextButtonProps = getJSIconButtonProps({
+    ...stylesJS.iconButtonOptions,
+    ariaLabel: 'Older',
+  })
+  const prevIconProps = getJSIconProps(prevButtonProps.iconOptions)
+  const nextIconProps = getJSIconProps(nextButtonProps.iconOptions)
+
+  return (
+    <div style={stylesJS.buttonGroup.styles}>
+      <button
+        disabled
+        style={{
+          ...prevButtonProps.button.styles,
+          ...prevButtonProps.button.styles['&:disabled'],
+        }}
+      >
+        <CaretLeftIcon
+          style={prevIconProps.styles}
+          {...prevIconProps.a11yProps}
+        />
+      </button>
+      <button style={nextButtonProps.button.styles}>
+        <CaretRightIcon
+          style={nextIconProps.styles}
+          {...nextIconProps.a11yProps}
+        />
+      </button>
+    </div>
+  )
+}
+
+function JSAllPagination() {
+  return (
+    <div style={stylesJS.container.styles}>
+      <JSPaginationInfo />
+      <JSSelect />
+      <JSArrowButtons />
     </div>
   )
 }
@@ -61,19 +232,23 @@ function AllPagination() {
 export default function Pagination() {
   return (
     <div id="pagination">
-      <h3>Pagination</h3>
+      <h2>Pagination</h2>
+      <h3>CSS API</h3>
       <div className="App-container column">
-        <div {...styles.container}>
-          <div>
-            <PaginationButtons />
-          </div>
-        </div>
+        <PaginationButtons />
       </div>
 
       <div className="App-container column">
-        <div {...styles.container}>
-          <AllPagination />
-        </div>
+        <AllPagination />
+      </div>
+
+      <h3>JS API</h3>
+      <div className="App-container column">
+        <JSPaginationButtons />
+      </div>
+
+      <div className="App-container column">
+        <JSAllPagination />
       </div>
     </div>
   )
