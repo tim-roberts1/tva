60ffb6b78895275e7c82e8badebe176112d2e77f
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-03-10 14:32:10 -0500

    fix(headless-styles): swap quotes in checkbox/radio JS APIs to match other outputs

--- packages/headless-styles/src/components/Checkbox/checkboxJS.ts
+++ packages/headless-styles/src/components/Checkbox/checkboxJS.ts
@@ -8,13 +8,13 @@ export function getJSCheckboxProps(options?: CheckboxOptions) {
   const props = createCheckboxProps(defaultOptions)
   const controlStyles = {
     ...styles.checkboxControl,
-    '&[data-checked="true"]:hover': {
+    "&[data-checked='true']:hover": {
       ...styles.checkboxControl_data_checked__true['&:hover'],
     },
-    '&[data-invalid="true"]:hover': {
+    "&[data-invalid='true']:hover": {
       ...styles.checkboxControl_data_invalid__true['&:hover'],
     },
-    '&[data-readonly="true"]:hover': {
+    "&[data-readonly='true']:hover": {
       ...styles.checkboxControl_data_readonly__true['&:hover'],
     },
   }
