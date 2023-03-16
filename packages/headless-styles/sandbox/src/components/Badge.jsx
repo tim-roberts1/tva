74de423233a0c7e4c06b9dd0a48075b3c10b365b
Casey Baggz <caseybaggz@gmail.com>
2023-03-16 09:41:57 -0500

    chore(sandbox): update Badge.jsx

--- packages/headless-styles/sandbox/src/components/Badge.jsx
+++ packages/headless-styles/sandbox/src/components/Badge.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { PlaceholderIcon, PencilIcon } from '@pluralsight/icons'
 import { getBadgeProps, getIconProps, getJSBadgeProps } from '../../../src'
 
@@ -27,61 +27,92 @@ function BadgeEl(props) {
   )
 }
 
-export default function Badge({ logJS }) {
-  useEffect(() => {
-    if (logJS) {
-      console.log({
-        ...getJSBadgeProps({
-          sentiment: 'action',
-          usage: 'outline',
-          size: 'xs',
-        }),
-      })
-    }
-  }, [logJS])
-
-  return (
-    <div id="badge">
-      <h3>Badge</h3>
-      <div className="App-container">
-        <BadgeEl sentiment="action">action</BadgeEl>
-        <BadgeEl>default</BadgeEl>
-        <BadgeEl usage="outline">outline</BadgeEl>
-      </div>
-
-      <div className="App-container">
-        <BadgeEl sentiment="action" size="xs">
-          xs action filled
-        </BadgeEl>
-        <BadgeEl usage="filled" size="xs">
-          xs default filled
-        </BadgeEl>
-        <BadgeEl usage="outline" size="xs">
-          xs default outline
-        </BadgeEl>
-      </div>
-
-      <div className="App-container">
-        <BadgeEl sentiment="action" icon={PlaceholderIcon}>
-          action
-        </BadgeEl>
-        <BadgeEl icon={PencilIcon}>default</BadgeEl>
-        <BadgeEl icon={PlaceholderIcon} usage="outline">
-          outline
-        </BadgeEl>
-      </div>
-
-      <div className="App-container">
-        <BadgeEl sentiment="action" size="xs" icon={PlaceholderIcon}>
-          xs action filled
-        </BadgeEl>
-        <BadgeEl usage="filled" size="xs">
-          xs default filled
-        </BadgeEl>
-        <BadgeEl usage="outline" size="xs">
-          xs default outline
-        </BadgeEl>
-      </div>
+const badgeContent = (
+  <div id="badge">
+    <h3>Badge</h3>
+    <div className="App-container">
+      <BadgeEl>default</BadgeEl>
+      <BadgeEl sentiment="success">success</BadgeEl>
+      <BadgeEl sentiment="warning">warning</BadgeEl>
+      <BadgeEl sentiment="danger">danger</BadgeEl>
+      <BadgeEl usage="outline">outline</BadgeEl>
     </div>
-  )
+
+    <div className="App-container">
+      <BadgeEl sentiment="action" size="xs">
+        xs action filled
+      </BadgeEl>
+      <BadgeEl usage="filled" size="xs">
+        xs default filled
+      </BadgeEl>
+      <BadgeEl usage="outline" size="xs">
+        xs default outline
+      </BadgeEl>
+    </div>
+
+    <div className="App-container">
+      <BadgeEl sentiment="action" icon={PlaceholderIcon}>
+        action
+      </BadgeEl>
+      <BadgeEl icon={PencilIcon}>default</BadgeEl>
+      <BadgeEl icon={PlaceholderIcon} usage="outline">
+        outline
+      </BadgeEl>
+    </div>
+
+    <div className="App-container">
+      <BadgeEl sentiment="action" size="xs" icon={PlaceholderIcon}>
+        xs action filled
+      </BadgeEl>
+      <BadgeEl usage="filled" size="xs">
+        xs default filled
+      </BadgeEl>
+      <BadgeEl usage="outline" size="xs">
+        xs default outline
+      </BadgeEl>
+    </div>
+
+    <h3>JS API</h3>
+    <div className="App-container">
+      <span
+        style={{
+          ...getJSBadgeProps({ sentiment: 'default' }).badge,
+        }}
+      >
+        default
+      </span>
+      <span
+        style={{
+          ...getJSBadgeProps({ sentiment: 'success' }).badge.styles,
+        }}
+      >
+        success
+      </span>
+      <span
+        style={{
+          ...getJSBadgeProps({ sentiment: 'warning' }).badge.styles,
+        }}
+      >
+        warning
+      </span>
+      <span
+        style={{
+          ...getJSBadgeProps({ sentiment: 'danger' }).badge.styles,
+        }}
+      >
+        danger
+      </span>
+      <span
+        style={{
+          ...getJSBadgeProps({ usage: 'outline' }).badge.styles,
+        }}
+      >
+        outline
+      </span>
+    </div>
+  </div>
+)
+
+export default function Badge() {
+  return badgeContent
 }
