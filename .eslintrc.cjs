043bc8d31417e55cb646ecfb9877c008ee031d20
Casey Baggz <caseybaggz@gmail.com>
2023-05-02 11:39:24 -0500

    chore: remove commented code in global .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,30 +7,18 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
-    // 'plugin:@typescript-eslint/recommended',
-    // 'plugin:react/recommended',
-    // 'plugin:react-hooks/recommended',
     'plugin:sonarjs/recommended',
     'plugin:jest/recommended',
     'plugin:jest-dom/recommended',
-    // 'plugin:testing-library/react',
-
     // Prettier must be last
     'prettier',
   ],
-  // parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    // '@typescript-eslint',
-    'sonarjs',
-    'jest',
-  ],
+  plugins: ['sonarjs', 'jest'],
   rules: {
-    // '@typescript-eslint/ban-ts-comment': OFF,
-    // '@typescript-eslint/no-var-requires': OFF,
     'default-case': ERROR,
     'default-case-last': ERROR,
     'dot-notation': [
@@ -45,30 +33,13 @@ module.exports = {
     'no-negated-condition': ERROR,
     'no-self-compare': ERROR,
     'no-template-curly-in-string': ERROR,
-    // 'react/jsx-uses-react': OFF,
-    // 'react/no-unknown-property': [ERROR, { ignore: ['tab-index'] }],
-    // 'react/prop-types': OFF,
-    // 'react/react-in-jsx-scope': OFF,
-    // 'react-refresh/only-export-components': WARN,
   },
   env: {
     jest: true,
     node: true,
   },
-  ignorePatterns: [
-    '.yarn/sdks',
-    // 'packages/*/build',
-    // 'packages/*/npm/*',
-    // 'packages/headless-styles/src/components/*/generated/*.module.ts',
-    'website/build',
-    'website/.yarn',
-  ],
+  ignorePatterns: ['.yarn/sdks', 'website/build', 'website/.yarn'],
   globals: {
     __EXPERIMENTAL__: 'readonly',
   },
-  // settings: {
-  //   react: {
-  //     version: 'detect',
-  //   },
-  // },
 }
