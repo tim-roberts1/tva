d20b5cec2aba489f2a04398f3e30bf77476460e2
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-05-11 14:37:54 -0400

    fix(headless-styles): update base classname for progress bar

--- packages/headless-styles/src/components/Progress/progressCSS.ts
+++ packages/headless-styles/src/components/Progress/progressCSS.ts
@@ -24,7 +24,7 @@ export function getProgressProps(options?: ProgressOptions) {
     bar: {
       ...a11yProps,
       ...createClassNameProp(
-        PROGRESS,
+        `${PROGRESS}-bar`,
         styles.bar,
         styles[sizeClass],
         styles[kind]
