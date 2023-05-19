9e0f1d502f91f1bde99a45b5ae8fd2dac0a79957
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-05-19 13:53:46 -0400

    test(headless-styles): remove unnecessary logging

--- packages/headless-styles/tests/scss/scss.test.ts
+++ packages/headless-styles/tests/scss/scss.test.ts
@@ -7,12 +7,7 @@ import { globSync } from 'glob'
 
 describe('Sass', () => {
   const testPath = `packages/headless-styles/tests/scss/*.test.scss`
-
-  console.log('Running Sass tests...', resolve(testPath))
-
   const testFiles = globSync(resolve(testPath))
 
-  console.log(`Running ${testFiles.length} Sass tests...`)
-  // Run on each file with describe() and it() functions
   testFiles.forEach((file) => runSass({ describe, it }, file))
 })
