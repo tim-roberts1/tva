ad4f349565f82a383256b66a1ee26ae3741b9026
Casey Baggz <caseybaggz@gmail.com>
2023-05-25 09:33:22 -0500

    docs: fix breaking UI change from HS update

--- website/src/components/DocsLinkBar/DocsLinkBar.js
+++ website/src/components/DocsLinkBar/DocsLinkBar.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { getGridProps, getGridItemProps } from '@pluralsight/headless-styles'
 import ViewSourceLink from '../ViewSourceLink/ViewSourceLink'
 import styles from './DocsLinkBar.module.css'
 
 export function DocsLinkItem(props) {
   return (
-    <li {...getGridItemProps({ colSpan: 3 })}>
+    <li className={styles.linkItem}>
       <ViewSourceLink {...props}>{props.children}</ViewSourceLink>
     </li>
   )
@@ -24,20 +23,13 @@ export function DocsFigmaLink(props) {
 }
 
 export function DocsLinkList(props) {
-  const { className, ...gridProps } = getGridProps({ cols: 12, gap: 8 })
-  return (
-    <ul className={`${className} ${styles.grid}`} {...gridProps}>
-      {props.children}
-    </ul>
-  )
+  return <ul className={styles.linkList}>{props.children}</ul>
 }
 
 export default function DocsLinkBar(props) {
   const { figma, github } = props
-  const { className, ...gridProps } = getGridProps({ cols: 12, gap: 8 })
-
   return (
-    <ul className={`${className} ${styles.grid}`} {...gridProps}>
+    <ul className={styles.linkList}>
       {github && <DocsGithubLink href={github} />}
       {figma && <DocsFigmaLink href={figma} />}
     </ul>
